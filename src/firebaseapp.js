012e00a9962515ff6cf06d71d59d004951a567f4
Ali, Ayman <ayman.ali@thermofisher.com>
2020-11-13 12:59:11 -0800

    override firebaseConfig to haj-mission-of-cote-de-ivoir

--- src/firebaseapp.js
+++ src/firebaseapp.js
@@ -12,5 +12,14 @@ if (localConfig) {
 firebaseConfig.authDomain = `${firebaseConfig.projectId}.firebaseapp.com`
 firebaseConfig.databaseURL = `${firebaseConfig.projectId}.firebaseio.com`
 firebaseConfig.storageBucket = `${firebaseConfig.projectId}.appspot.com`
+ firebaseConfig = {
+    apiKey: "AIzaSyBsDMoODcVcS0SB-hHrsbevrHG7x45wpjo",
+    authDomain: "hajj-mission-of-cote-de-ivoir.firebaseapp.com",
+    databaseURL: "https://hajj-mission-of-cote-de-ivoir.firebaseio.com",
+    projectId: "hajj-mission-of-cote-de-ivoir",
+    storageBucket: "hajj-mission-of-cote-de-ivoir.appspot.com",
+    messagingSenderId: "140613751017",
+    appId: "1:140613751017:web:4dd1f64e4b6fc55c2fa078"
+  };
 firebase.initializeApp(firebaseConfig);
 export default firebase;
