0cb9a7084577683d5c714157af4a789371a48223
Ali, Ayman <ayman.ali@thermofisher.com>
2020-11-18 17:56:19 -0800

    base breno, deploy to haj mision

--- src/firebaseapp.js
+++ src/firebaseapp.js
@@ -1,24 +1,4 @@
 import firebase from 'firebase';
- const firebaseConfig = {
-    apiKey: "AIzaSyBsDMoODcVcS0SB-hHrsbevrHG7x45wpjo",
-    authDomain: "hajj-mission-of-cote-de-ivoir.firebaseapp.com",
-    databaseURL: "https://hajj-mission-of-cote-de-ivoir.firebaseio.com",
-    projectId: "hajj-mission-of-cote-de-ivoir",
-    storageBucket: "hajj-mission-of-cote-de-ivoir.appspot.com",
-    messagingSenderId: "140613751017",
-    appId: "1:140613751017:web:4dd1f64e4b6fc55c2fa078"
-  };
-
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyDfbYiKtFPRzxJbOZHdvrVibtcqVUhfzlo",
-  //   authDomain: "breno1-81c45.firebaseapp.com",
-  //   databaseURL: "https://breno1-81c45.firebaseio.com",
-  //   projectId: "breno1-81c45",
-  //   storageBucket: "breno1-81c45.appspot.com",
-  //   messagingSenderId: "413195855317",
-  //   appId: "1:413195855317:web:6f916414b9970d3534095c"
-  // };
-
-  // TODO: Make the base project generic instead of hajj mission of cote de ivoire
+import { firebaseConfig } from './firebaseConfig';
 firebase.initializeApp(firebaseConfig);
 export default firebase;
