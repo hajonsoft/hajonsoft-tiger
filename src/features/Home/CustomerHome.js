c65f638f3dbfadc05e0671bce848532ade3585a4
Ali, Ayman <ayman.ali@thermofisher.com>
2020-12-12 19:07:23 -0800

    welcome

--- src/features/Home/CustomerHome.js
+++ src/features/Home/CustomerHome.js
@@ -1,8 +1,9 @@
 import { Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import { Helmet } from "react-helmet";
-import  firebaseConfig  from '../../firebaseConfig';
-
+import firebaseConfig from '../../firebaseConfig';
+import _ from 'lodash'
+import welcome from '../../images/welcome.jpg'
 const CustomerHome = () => {
 
 
@@ -21,16 +22,18 @@ const CustomerHome = () => {
                 <title>{firebaseConfig.projectId}</title>
             </Helmet>
             <Paper elevation={4} style={{ padding: '4rem', height: '60vh' }}>
-                <Grid container justify="center" alignItems="center">
-                    <Grid item>
-                        <Typography variant="h6">{`Welcome to ${firebaseConfig.projectId} traveller portal. You will need your passport number and expire date to edit your record.`}</Typography>
+                <Grid container justify="center" alignItems="space-between" style={{height: '60vh'}}>
+                    <Grid item xs={12}>
+                        <Typography variant="h2" align="center">{`${_.startCase(firebaseConfig.projectId.replace(/[0-9]/g, '').replace(/-/g, ' '))}`}</Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="h6">If you are an admin, <a href="/admin">click here to login.</a></Typography>
+                        <img src={welcome} alt="welcome" />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="body1" align="center">If you are an admin, <a href="/admin">click here to login.</a></Typography>
                     </Grid>
                 </Grid>
             </Paper>
-            {/* <Button onClick={handleDoit}>Do it</Button> */}
         </div>
     )
 };
