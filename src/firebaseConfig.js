68abb990b4ef8efa64113fe32451f4647f26a84f
aali-globality <ayman.ali@globality-contractor.com>
2021-05-26 18:03:37 -0700

    restore package.json

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,6 @@
-// import firebaseConfig from './firebaseConfigs/hajj-mission-of-cote-de-ivoir'; 
+import firebaseConfig from './firebaseConfigs/hajj-mission-of-cote-de-ivoir'; 
 // import firebaseConfig from './firebaseConfigs/breno1-81c45';
-import firebaseConfig from './firebaseConfigs/forhajjnow';
+// import firebaseConfig from './firebaseConfigs/forhajjnow';
 // import firebaseConfig from './firebaseConfigs/hajonsoft2020';
 
 export default firebaseConfig;
