56880738e0e577f4b03d7b8be04a559fdd46ec7a
aali-globality <ayman.ali@globality-contractor.com>
2021-06-03 23:49:59 -0700

    cohohadj-travel2021

---
+++ src/firebaseConfigs/cohohadj-travel2021.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtue408pl9_79guR3RsuQ2U-fEpQee_qs",
+  authDomain: "cohohadj-travel2021.firebaseapp.com",
+  databaseURL: "https://cohohadj-travel2021-default-rtdb.firebaseio.com",
+  projectId: "cohohadj-travel2021",
+  storageBucket: "cohohadj-travel2021.appspot.com",
+  messagingSenderId: "603823085803",
+  appId: "1:603823085803:web:d40c8f25503525c4ae46ae"
+};
+
+export default firebaseConfig;
