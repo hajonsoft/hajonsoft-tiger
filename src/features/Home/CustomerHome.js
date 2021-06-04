fb0c7a012d98d60cc67f764920f53b06253e777e
aali-globality <ayman.ali@globality-contractor.com>
2021-06-04 00:06:26 -0700

    Coho deploy

--- src/features/Home/CustomerHome.js
+++ src/features/Home/CustomerHome.js
@@ -1,9 +1,10 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Grid } from '@material-ui/core';
 import React from 'react';
 import roundedPlus from '../../images/customer-home-background.svg';
 import PackagesContainer from '../onlinePackage/components/PackagesContainer';
 import CustomerHeader from './../Header/CustomerHeader';
 import ImportantDates from './components/importantDates';
+import hummingBird from '../../images/humming-bird.svg'
 
 const CustomerHome = () => {
 
@@ -16,7 +17,15 @@ const CustomerHome = () => {
                 <PackagesContainer />
             </Box>
             <Box m={4}>
-                <Typography variant="body1" align="center" style={{ color: '#18a600' }}>If you are a dove admin, <a style={{ color: '#18a600' }} href="/admin">click here to login to Hummingbird</a></Typography>
+                <Grid container justify="center" alignItems="center" spacing={2}>
+                    <Grid item>
+                        <Typography variant="body1" align="center" style={{ color: '#18a600' }}>If you are a admin! <a style={{ color: '#18a600' }} href="/admin">click here to login to Hummingbird</a></Typography>
+                    </Grid>
+                    <Grid item>
+                        <img src={hummingBird} alt="humming bird" width="64" height="64"></img>
+
+                    </Grid>
+                </Grid>
             </Box>
 
         </div >
