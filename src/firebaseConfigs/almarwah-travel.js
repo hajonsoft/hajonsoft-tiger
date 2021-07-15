88fcc72b10fead4096f190b3595892cb77e788c3
aali-nutrien <aliayman@nutrien.com>
2021-07-15 10:34:26 -0700

    fix: signup almarwah-travel

---
+++ src/firebaseConfigs/almarwah-travel.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "almarwah-travel",
+  "appId": "1:690432690240:web:3f8ec9713ae9b2bca437a5",
+  "databaseURL": "https://almarwah-travel-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "almarwah-travel.appspot.com",
+  "locationId": "australia-southeast1",
+  "apiKey": "AIzaSyDtEO8aKtCyeWuyEh25MgBGZX4F7QAx1Vw",
+  "authDomain": "almarwah-travel.firebaseapp.com",
+  "messagingSenderId": "690432690240"
+};
+export default firebaseConfig;
+
