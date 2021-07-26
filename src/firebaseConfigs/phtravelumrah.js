5e418e8d85b4cb651205776e6884d9587354e8fb
aali-nutrien <aliayman@nutrien.com>
2021-07-26 08:22:52 -0700

    feat: ph travel signup

---
+++ src/firebaseConfigs/phtravelumrah.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "phtravelumrah",
+  "appId": "1:987680672641:web:3e6c3babd4c99e721ab680",
+  "databaseURL": "https://phtravelumrah-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "phtravelumrah.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAkZpsebERrZGbD3dU7wY_F12eD6Qv6l5U",
+  "authDomain": "phtravelumrah.firebaseapp.com",
+  "messagingSenderId": "987680672641"
+};
+export default firebaseConfig;
+
