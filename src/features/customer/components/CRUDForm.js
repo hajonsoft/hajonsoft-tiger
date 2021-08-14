9a2d45250714b397681b4453de92647c3594256a
aali-bright <ayman@brightlive.com>
2021-08-14 13:57:32 -0700

    feat: show reservation name

--- src/features/customer/components/CRUDForm.js
+++ src/features/customer/components/CRUDForm.js
@@ -17,6 +17,7 @@ import RecentActorsOutlinedIcon from "@material-ui/icons/RecentActorsOutlined";
 import SaveOutlinedIcon from "@material-ui/icons/SaveOutlined";
 import TranslateIcon from "@material-ui/icons/Translate";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import Alert from "@material-ui/lab/Alert";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import { Form, Formik } from "formik";
@@ -231,6 +232,14 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
     window.open(url, "_blank");
   };
 
+  const handleAcceptOnlineReservation = (data) => {
+    createTraveller({ path: `customer/${record.packageName}`, data });
+    deleteTraveller({
+      path: `customer/online/${record._fid}`
+    });
+    onClose()
+  };
+
   const getFullArabicName = (arabicNameDictionary) => {
     const cursor = Object.values(arabicNameDictionary);
     let fullArabicName = "";
@@ -280,11 +289,11 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
         initialValues={
           mode === "create"
             ? {
-                nationality: popularNationality(),
-                passExpireDt: moment().add(6, "month"),
-                passIssueDt: moment().subtract(7, "days"),
-                birthDate: moment().subtract(7, "days"),
-              }
+              nationality: popularNationality(),
+              passExpireDt: moment().add(6, "month"),
+              passIssueDt: moment().subtract(7, "days"),
+              birthDate: moment().subtract(7, "days"),
+            }
             : record
         }
         validationSchema={mode !== "delete" ? validSchema : null}
@@ -292,6 +301,20 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
       >
         {({ setFieldValue, values, errors, isSubmitting, touched }) => (
           <Form>
+            {packageName === 'online' && record.packageName &&
+
+              <Alert severity="info" style={{ width: '100%' }}>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                  <Typography variant="h5" >
+                    {record.packageName}
+                  </Typography>
+                  <div style={{marginLeft: '2rem'}}>
+                    <Button color="primary" size="small" variant="contained" onClick={() => handleAcceptOnlineReservation(values)} >Accept Reservation</Button>
+                  </div>
+                </div>
+
+              </Alert>
+            }
             <Card raised className={classes.formContainer}>
               <CardHeader
                 className={classes.cardTitle}
@@ -328,7 +351,7 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
                         />
                       )}
                       {mode === "create" && (
-                        <Grid container item xs style={{padding: '1rem'}}>
+                        <Grid container item xs style={{ padding: '1rem' }}>
                           <Dropzone
                             onClose={onClose}
                             saveToFirebase={handleSubmitForm}
@@ -351,7 +374,7 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
                         justify="space-around"
                       >
                         <Grid container spacing={3} alignItems="center">
-                          <Grid item xs={10}>
+                          <Grid item xs={9}>
                             <InputControl
                               name="name"
                               label={trans("reservation.full-name")}
@@ -361,25 +384,25 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
                               helperText={touched.name && errors.name}
                             />
                           </Grid>
-                          <Grid item xs={2}>
+                          <Grid item xs={3}>
                             <IconButton>
-                              <FacebookIcon
+                              <FacebookIcon fontSize="small"
                                 onClick={() => handleFacebookClick(values.name)}
                               />
                             </IconButton>
                             <IconButton>
-                              <TwitterIcon
+                              <TwitterIcon fontSize="small"
                                 onClick={() => handleTwitterClick(values.name)}
                               />
                             </IconButton>
-                            <IconButton>
-                              <FontAwesomeIcon
+                            <IconButton >
+                              <FontAwesomeIcon size="sm"
                                 icon={faGoogle}
                                 onClick={() => handleGoogleClick(values.name)}
                               />
                             </IconButton>
                           </Grid>
-                          <Grid item xs={10}>
+                          <Grid item xs={9}>
                             <InputControl
                               name="nameArabic"
                               label={trans("reservation.arabic-name")}
@@ -393,7 +416,7 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
                               required={false}
                             />
                           </Grid>
-                          <Grid item xs={2}>
+                          <Grid item xs={3}>
                             <IconButton>
                               <TranslateIcon
                                 onClick={() =>
@@ -671,8 +694,8 @@ const CRUDForm = ({ mode, record, customerKey, title, onClose, onNext }) => {
                         style={{ color: "#f44336" }}
                       >
                         {errors &&
-                        Object.keys(errors).length === 0 &&
-                        errors.constructor === Object
+                          Object.keys(errors).length === 0 &&
+                          errors.constructor === Object
                           ? ""
                           : JSON.stringify(errors).replace(/"/g, "")}
                       </Typography>
