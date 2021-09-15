10d9e02bdd92fcddfdc9aa9c2f055340378f2913
Breno Capaz <brenocapaz@gmail.com>
2021-09-15 08:57:00 -0700

    makkahtoursonline

---
+++ src/firebaseConfigs/makkahtoursonline.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "makkahtoursonline",
+  "appId": "1:505408091167:web:93107db564e4fc631cd12a",
+  "databaseURL": "https://makkahtoursonline-default-rtdb.firebaseio.com",
+  "storageBucket": "makkahtoursonline.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDUdviX1k6_QNWFHZbJrOkV1JVxSQEJDdM",
+  "authDomain": "makkahtoursonline.firebaseapp.com",
+  "messagingSenderId": "505408091167"
+};
+export default firebaseConfig;
+
