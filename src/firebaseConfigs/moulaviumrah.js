ce67d1b061dca49f3ea5d6aa8d7f7d8b35c2e929
aali-bright <ayman@brightlive.com>
2021-09-15 06:35:00 -0700

    moulaviumrah

---
+++ src/firebaseConfigs/moulaviumrah.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "moulaviumrah",
+  "appId": "1:454386397564:web:0917cf6855d3941e8e74f9",
+  "databaseURL": "https://moulaviumrah-default-rtdb.firebaseio.com",
+  "storageBucket": "moulaviumrah.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyD6cih2jSccw1BfHpvrnAxyPPlRxVG0v5I",
+  "authDomain": "moulaviumrah.firebaseapp.com",
+  "messagingSenderId": "454386397564"
+};
+export default firebaseConfig;
+
