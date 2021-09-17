b172c4b6a8eafef1d7d1ba3d7102329ea860bd88
aali-bright <ayman@brightlive.com>
2021-09-17 07:49:36 -0700

    omra-2021-2022

---
+++ src/firebaseConfigs/omra-2021-2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "omra-2021-2022",
+  "appId": "1:223659922800:web:dec75306c9ccd08cf82bb5",
+  "databaseURL": "https://omra-2021-2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "omra-2021-2022.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBBhRFS2NG_Hgq8egqWJhuw5Gqipbn0qUg",
+  "authDomain": "omra-2021-2022.firebaseapp.com",
+  "messagingSenderId": "223659922800"
+};
+export default firebaseConfig;
+
