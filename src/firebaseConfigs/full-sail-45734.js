b218ef7d52b09a75ee7e7bb2467546530390525a
aali-bright <ayman@brightlive.com>
2021-09-18 11:13:55 -0700

    full-sail-45734

---
+++ src/firebaseConfigs/full-sail-45734.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "full-sail-45734",
+  "appId": "1:888870845247:web:516b944056949d3f5d318a",
+  "databaseURL": "https://full-sail-45734-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "full-sail-45734.appspot.com",
+  "locationId": "asia-south1",
+  "apiKey": "AIzaSyDUzQP_Ytst_Ooxr4PCaQSJVKPtJS73qRQ",
+  "authDomain": "full-sail-45734.firebaseapp.com",
+  "messagingSenderId": "888870845247"
+};
+export default firebaseConfig;
+
