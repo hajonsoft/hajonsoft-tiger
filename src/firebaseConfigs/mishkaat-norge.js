2449425a80c1b3ddcbcc6e3ee2c525423ee2ddc6
Breno Capaz <brenocapaz@gmail.com>
2021-09-27 14:19:29 -0700

    mishkaat-norge

---
+++ src/firebaseConfigs/mishkaat-norge.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "mishkaat-norge",
+  "appId": "1:859016865207:web:c294404c9169f936acb432",
+  "databaseURL": "https://mishkaat-norge-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "mishkaat-norge.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBfzJBF4G8vap5MisLT4lYDyihzJfyAE54",
+  "authDomain": "mishkaat-norge.firebaseapp.com",
+  "messagingSenderId": "859016865207"
+};
+export default firebaseConfig;
+
