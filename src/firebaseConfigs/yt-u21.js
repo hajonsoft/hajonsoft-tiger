76030883d551950c0ab81d9d98da7dc5f46f5793
aali-bright <ayman@brightlive.com>
2021-09-28 09:16:02 -0700

    yt-u21

---
+++ src/firebaseConfigs/yt-u21.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "yt-u21",
+  "appId": "1:1040438237862:web:abd3309b47ce88c75ef267",
+  "databaseURL": "https://yt-u21-default-rtdb.firebaseio.com",
+  "storageBucket": "yt-u21.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyB3guiYYb48DPuf_EdONRwNUrTvcvCBkFc",
+  "authDomain": "yt-u21.firebaseapp.com",
+  "messagingSenderId": "1040438237862"
+};
+export default firebaseConfig;
+
