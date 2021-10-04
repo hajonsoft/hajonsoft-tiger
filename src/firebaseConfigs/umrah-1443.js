6a74f418249aefab59399d6b2dd7aa21d060f913
aali-bright <ayman@brightlive.com>
2021-10-04 17:40:51 +0200

    umrah-1443

---
+++ src/firebaseConfigs/umrah-1443.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "umrah-1443",
+  "appId": "1:577255437119:web:0753446958053c2bcd198a",
+  "databaseURL": "https://umrah-1443-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "umrah-1443.appspot.com",
+  "locationId": "europe-central2",
+  "apiKey": "AIzaSyATi_BfD3qqjq_48eQZrA3b1_ZeeswDpcE",
+  "authDomain": "umrah-1443.firebaseapp.com",
+  "messagingSenderId": "577255437119",
+  "measurementId": "G-2P60PKV5GX"
+};
+export default firebaseConfig;
+
