f68b454ef3a2f73708fb51ec249ba719538b315b
aali-bright <ayman@brightlive.com>
2021-10-26 05:48:57 -0700

    ocean-travel-14092

---
+++ src/firebaseConfigs/ocean-travel-14092.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "ocean-travel-14092",
+  "appId": "1:631522009927:web:59fd2814b8ca68de156262",
+  "databaseURL": "https://ocean-travel-14092-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "ocean-travel-14092.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBMdzL4e1aGpgq8MkDyQbS3bbVwKVVpXqI",
+  "authDomain": "ocean-travel-14092.firebaseapp.com",
+  "messagingSenderId": "631522009927",
+  "measurementId": "G-PW77135NKG"
+};
+export default firebaseConfig;
+
