42bc1fc08a265d5722b7c7b3d255d43c6075355e
aali-bright <ayman@brightlive.com>
2021-10-29 08:43:26 -0700

    safar-omra-france

---
+++ src/firebaseConfigs/safar-omra-france.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "safar-omra-france",
+  "appId": "1:848220632853:web:9d43fb606b04eafb225b1c",
+  "databaseURL": "https://safar-omra-france-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "safar-omra-france.appspot.com",
+  "locationId": "europe-west6",
+  "apiKey": "AIzaSyDKBfsy9EfmmXfpKT3Px6K9hZjSx_59jCQ",
+  "authDomain": "safar-omra-france.firebaseapp.com",
+  "messagingSenderId": "848220632853"
+};
+export default firebaseConfig;
+
