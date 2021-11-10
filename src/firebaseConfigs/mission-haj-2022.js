4f8872843266304fd661aebfb8e448d930f02437
aali-bright <ayman@brightlive.com>
2021-11-09 16:45:05 -0800

    mission-haj-2022

---
+++ src/firebaseConfigs/mission-haj-2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "mission-haj-2022",
+  "appId": "1:985397375857:web:eabd3b4b121d517c56fcd0",
+  "databaseURL": "https://mission-haj-2022-default-rtdb.firebaseio.com",
+  "storageBucket": "mission-haj-2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAdA5EMVOhjQTXEtvrvERaW80pDchi_huE",
+  "authDomain": "mission-haj-2022.firebaseapp.com",
+  "messagingSenderId": "985397375857"
+};
+export default firebaseConfig;
+
