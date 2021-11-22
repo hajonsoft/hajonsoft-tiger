847084897e13d660b791f4d21ef8f5168f2eef01
aali-bright <ayman@brightlive.com>
2021-11-22 13:45:38 -0800

    classyvisatour

---
+++ src/firebaseConfigs/classyvisatour.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "classyvisatour",
+  "appId": "1:430519706083:web:c6b9eb1ba0c247a9dfc0d6",
+  "databaseURL": "https://classyvisatour-default-rtdb.firebaseio.com",
+  "storageBucket": "classyvisatour.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAkdq9QCg64hVwJ3n9X5Dv-d95sHYQFfEw",
+  "authDomain": "classyvisatour.firebaseapp.com",
+  "messagingSenderId": "430519706083"
+};
+export default firebaseConfig;
+
