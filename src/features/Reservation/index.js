b74ab5f20f1b9dc7b32908c485784852fb6fa8b7
Babatunde50 <babatundeololade50@gmail.com>
2021-11-23 19:50:31 +0300

    add description to learn more

--- src/features/Reservation/index.js
+++ src/features/Reservation/index.js
@@ -1,18 +1,25 @@
-import { Box, Button, Grid, MenuItem, Select, Typography } from "@material-ui/core";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import Modal from "@material-ui/core/Modal";
-import { makeStyles } from "@material-ui/core/styles";
-import Tab from "@material-ui/core/Tab";
-import Tabs from "@material-ui/core/Tabs";
-import ExploreIcon from "@material-ui/icons/Explore";
-import FlightTakeoffIcon from "@material-ui/icons/FlightTakeoff";
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import firebase from "../../firebaseapp";
-import trans from "../../shared/util/trans";
-import BasicReservation from "./components/BasicReservation";
-import FullReservation from "./components/FullReservation";
+import {
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import ExploreIcon from '@material-ui/icons/Explore';
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import firebase from '../../firebaseapp';
+import trans from '../../shared/util/trans';
+import BasicReservation from './components/BasicReservation';
+import FullReservation from './components/FullReservation';
 import reservationCompleteImage from '../../images/reservation-complete.svg';
 import t from '../../shared/util/trans';
 
@@ -20,60 +27,63 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    display: "flex",
+    display: 'flex',
     height: 224,
   },
   tabs: {
     borderRight: `1px solid white`,
   },
   modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid whiite",
+    border: '2px solid whiite',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    minWidth: "50%"
+    minWidth: '50%',
   },
   paymentBtnContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "2rem 0px"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '2rem 0px',
   },
   paymentBtn: {
-    background: "#178CF9",
-    paddingLeft: "3rem",
-    paddingRight: "3rem",
-    paddingTop: ".65rem",
-    paddingBottom: ".65rem",
-    textTransform: "capitalize",
-    color: "white",
+    background: '#178CF9',
+    paddingLeft: '3rem',
+    paddingRight: '3rem',
+    paddingTop: '.65rem',
+    paddingBottom: '.65rem',
+    textTransform: 'capitalize',
+    color: 'white',
   },
   viewReservationBtn: {
-    paddingLeft: "3rem",
-    paddingRight: "3rem",
-    marginRight: "1rem",
-    paddingTop: ".65rem",
-    paddingBottom: ".65rem",
-    textTransform: "capitalize",
+    paddingLeft: '3rem',
+    paddingRight: '3rem',
+    marginRight: '1rem',
+    paddingTop: '.65rem',
+    paddingBottom: '.65rem',
+    textTransform: 'capitalize',
   },
 }));
 
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
   };
 }
 
 const Reservation = ({ lang, onLanguageChange }) => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
   const params = useParams();
+  const history = useHistory();
+  const [value, setValue] = React.useState(
+    history.location.search.split('=')[1] === 'true' ? 1 : 0
+  );
   const [open, setOpen] = React.useState(false);
 
   const [advertisementData, setAdvertisementData] = React.useState({});
@@ -81,17 +91,20 @@ const Reservation = ({ lang, onLanguageChange }) => {
   useEffect(() => {
     async function getPackageSnapshot() {
       if (params.packageName) {
-        const allAdvertisementsSnapshot = await firebase.database().ref(`protected/onlinePackage`).once('value');
+        const allAdvertisementsSnapshot = await firebase
+          .database()
+          .ref(`protected/onlinePackage`)
+          .once('value');
         const allAdvertisments = allAdvertisementsSnapshot.val();
         const allAdvertismentsKeys = Object.keys(allAdvertisments);
         for (const advertisementKey of allAdvertismentsKeys) {
           if (allAdvertisments[advertisementKey].name === params.packageName) {
-            setAdvertisementData(allAdvertisments[advertisementKey])
+            setAdvertisementData(allAdvertisments[advertisementKey]);
             return;
           }
         }
       }
-    };
+    }
     getPackageSnapshot();
   }, [params.packageName]);
 
@@ -109,7 +122,7 @@ const Reservation = ({ lang, onLanguageChange }) => {
 
   return (
     <>
-      <Grid container style={{ backgroundColor: "#ccc", minHeight: "100vh" }}>
+      <Grid container style={{ backgroundColor: '#ccc', minHeight: '100vh' }}>
         <Grid item xs={3}>
           <Tabs
             orientation="vertical"
@@ -120,7 +133,7 @@ const Reservation = ({ lang, onLanguageChange }) => {
             textColor="primary"
           >
             <Tab
-              style={{ width: "100%" }}
+              style={{ width: '100%' }}
               label={
                 <Grid
                   container
@@ -131,13 +144,13 @@ const Reservation = ({ lang, onLanguageChange }) => {
                   <Grid item>
                     <ExploreIcon fontSize="small" />
                   </Grid>
-                  <Grid item>{trans("reservation.quick-reservation")}</Grid>
+                  <Grid item>{trans('reservation.quick-reservation')}</Grid>
                 </Grid>
               }
               {...a11yProps(0)}
             />
             <Tab
-              style={{ width: "100%" }}
+              style={{ width: '100%' }}
               label={
                 <Grid
                   container
@@ -148,13 +161,13 @@ const Reservation = ({ lang, onLanguageChange }) => {
                   <Grid item>
                     <FlightTakeoffIcon fontSize="small" />
                   </Grid>
-                  <Grid item>{trans("reservation.full-reservation")}</Grid>
+                  <Grid item>{trans('reservation.full-reservation')}</Grid>
                 </Grid>
               }
               {...a11yProps(1)}
             />
           </Tabs>
-          <Box p={4} style={{ display: "flex", justifyContent: "center" }}>
+          <Box p={4} style={{ display: 'flex', justifyContent: 'center' }}>
             <Select
               value={lang}
               onChange={(e) => onLanguageChange(e.target.value)}
@@ -173,7 +186,14 @@ const Reservation = ({ lang, onLanguageChange }) => {
           </Box>
         </Grid>
         <Grid item xs={9}>
-          {value === 0 ? <BasicReservation openSuccessModal={handleOpen} isModalOpen={open} /> : <FullReservation openSuccessModal={handleOpen} isModalOpen={open} />}
+          {value === 0 ? (
+            <BasicReservation
+              openSuccessModal={handleOpen}
+              isModalOpen={open}
+            />
+          ) : (
+            <FullReservation openSuccessModal={handleOpen} isModalOpen={open} />
+          )}
         </Grid>
       </Grid>
       <Modal
@@ -190,13 +210,35 @@ const Reservation = ({ lang, onLanguageChange }) => {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <h2 style={{ textAlign: "center" }} >A Reservation has been booked for you</h2>
-            <div style={{ width: "25%", height: 150, margin: "1rem auto" }}>
-              <img src={reservationCompleteImage} alt="success-icon" style={{ width: "100%", height: "100%" }} />
+            <h2 style={{ textAlign: 'center' }}>
+              A Reservation has been booked for you
+            </h2>
+            <div style={{ width: '25%', height: 150, margin: '1rem auto' }}>
+              <img
+                src={reservationCompleteImage}
+                alt="success-icon"
+                style={{ width: '100%', height: '100%' }}
+              />
             </div>
             <div className={classes.paymentBtnContainer}>
-              <Button color="default" variant="contained" className={classes.viewReservationBtn} onClick={handleClose} >{t('view-reservation-number')}</Button>
-              {advertisementData.paymentLink && <Button color="primary" variant="contained" className={classes.paymentBtn} href={advertisementData.paymentLink} >{t('continue-to-payment')}</Button>}
+              <Button
+                color="default"
+                variant="contained"
+                className={classes.viewReservationBtn}
+                onClick={handleClose}
+              >
+                {t('view-reservation-number')}
+              </Button>
+              {advertisementData.paymentLink && (
+                <Button
+                  color="primary"
+                  variant="contained"
+                  className={classes.paymentBtn}
+                  href={advertisementData.paymentLink}
+                >
+                  {t('continue-to-payment')}
+                </Button>
+              )}
             </div>
           </div>
         </Fade>
