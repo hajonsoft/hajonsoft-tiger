dccda2d31cfc9a0370a8099f7bc11d25e7f70ff7
aali-bright <ayman@brightlive.com>
2021-12-30 08:33:42 -0800

    visa-mkm2022

---
+++ src/firebaseConfigs/visa-mkm2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "visa-mkm2022",
+  "appId": "1:429200252004:web:a6d500cbaee8bda5f79371",
+  "databaseURL": "https://visa-mkm2022-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "visa-mkm2022.appspot.com",
+  "locationId": "asia-southeast1",
+  "apiKey": "AIzaSyAXeOTgbr24W0qmr34DhT6a4cnghveTqJs",
+  "authDomain": "visa-mkm2022.firebaseapp.com",
+  "messagingSenderId": "429200252004"
+};
+export default firebaseConfig;
+
