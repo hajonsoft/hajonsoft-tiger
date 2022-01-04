2b1d274b135977beb220fbb7de39d596b67a64ee
aali-bright <ayman@brightlive.com>
2022-01-04 06:00:16 -0800

    azaan-travel-aps

---
+++ src/firebaseConfigs/azaan-travel-aps.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "azaan-travel-aps",
+  "appId": "1:774866988671:web:526b9ea51acf2d0d7e594a",
+  "databaseURL": "https://azaan-travel-aps-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "azaan-travel-aps.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBAUQNJfaXRo3zGfibdXkaGMJnXWbwKMhM",
+  "authDomain": "azaan-travel-aps.firebaseapp.com",
+  "messagingSenderId": "774866988671"
+};
+export default firebaseConfig;
+
