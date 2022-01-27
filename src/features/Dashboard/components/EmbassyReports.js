5c1ed10b7826f6d58beed6e7a4fe29cf6bf85faa
Babatunde50 <babatundeololade50@gmail.com>
2022-01-27 10:03:13 +0300

    add mofa barcode

--- src/features/Dashboard/components/EmbassyReports.js
+++ src/features/Dashboard/components/EmbassyReports.js
@@ -7,43 +7,45 @@ import {
   TextField,
   Button,
   Modal,
-} from "@material-ui/core";
-import React, { useRef, useState } from "react";
-import Table from "./Table";
-import ReactToPrint from "react-to-print";
-import Edit from "@material-ui/icons/Edit";
-import PrintableTable from "./PrintableTable";
-import { createReport } from "../../Dashboard/redux/reportSlice";
-import { useDispatch } from "react-redux";
-import { PrintOutlined, SaveOutlined } from "@material-ui/icons";
-import moment from "moment";
-import _ from "lodash";
-import { nationalities } from "../../../data/nationality";
+} from '@material-ui/core';
+import React, { useRef, useState } from 'react';
+import Table from './Table';
+import ReactToPrint from 'react-to-print';
+import Edit from '@material-ui/icons/Edit';
+import PrintableTable from './PrintableTable';
+import { createReport } from '../../Dashboard/redux/reportSlice';
+import { useDispatch } from 'react-redux';
+import { PrintOutlined, SaveOutlined } from '@material-ui/icons';
+import moment from 'moment';
+import _ from 'lodash';
+import { nationalities } from '../../../data/nationality';
+import Barcode from 'react-barcode'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
+    width: '100%',
   },
   paper: {
-    position: "absolute",
+    position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #fff",
+    border: '2px solid #fff',
     borderRadius: 5,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
     left: 0,
     right: 0,
     top: 50,
-    marginLeft: "auto",
-    marginRight: "auto",
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   title: {
     fontSize: 18,
-    fontWeight: "bold",
-    paddingBottom: "1rem",
+    fontWeight: 'bold',
+    paddingBottom: '1rem',
   },
 }));
+
 
 const EmbassyReports = ({ passengers }) => {
   const [data, setData] = useState(formatPassengers(passengers));
@@ -51,75 +53,90 @@ const EmbassyReports = ({ passengers }) => {
   function formatPassengers(data) {
     const formattedPassengers = [];
     for (let i = 0; i < data.length; i++) {
-      const nameParts = data[i].name.split(" ");
-      const nationality = nationalities?.find(nationalityRecord => nationalityRecord.name === passengers?.[i]?.nationality)?.code;
-      const issueCountry = passengers?.[i]?.codeLine?.substring(2,5) || nationality;
-      console.log('%cMyProject%cline:57%cpassengers[i]', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(3, 101, 100);padding:3px;border-radius:2px', passengers[i])
+      const nameParts = data[i].name.split(' ');
+      const nationality = nationalities?.find(
+        (nationalityRecord) =>
+          nationalityRecord.name === passengers?.[i]?.nationality
+      )?.code;
+      const issueCountry =
+        passengers?.[i]?.codeLine?.substring(2, 5) || nationality;
+      console.log(
+        '%cMyProject%cline:57%cpassengers[i]',
+        'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
+        'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
+        'color:#fff;background:rgb(3, 101, 100);padding:3px;border-radius:2px',
+        passengers[i]
+      );
       formattedPassengers.push({
         seq: i + 1,
         ...data[i],
-        birthDate: moment(data[i].birthDate).format("D-MMM-YYYY"),
+        birthDate: moment(data[i].birthDate).format('D-MMM-YYYY'),
         idNumberIssueDate: moment(data[i].idNumberIssueDate).format(
-          "D-MMM-YYYY"
+          'D-MMM-YYYY'
         ),
         idNumberExpireDate: moment(data[i].idNumberExpireDate).format(
-          "D-MMM-YYYY"
+          'D-MMM-YYYY'
         ),
-        passExpireDt: moment(data[i].passExpireDt).format("D-MMM-YYYY"),
-        passIssueDt: moment(data[i].passIssueDt).format("D-MMM-YYYY"),
+        passExpireDt: moment(data[i].passExpireDt).format('D-MMM-YYYY'),
+        passIssueDt: moment(data[i].passIssueDt).format('D-MMM-YYYY'),
         amadeusNM: `NM1${_.head(nameParts)}/${_.last(nameParts)} ${gTitle(
           data[i]
         )}`,
-        saber_: `-${_.head(nameParts)}/${_.last(nameParts)} ${gTitle(
-          data[i]
-        )}`,
-        amadeusSRDOC: `SRDOCSXXHK1-P-${issueCountry}-${passengers?.[i]?.passportNumber}-${nationality}-${moment(passengers?.[i]?.birthDate).format(
-          "DDMMMYY")}-${passengers?.[i]?.gender?.substring(0, 1)}-${moment(passengers?.[i]?.passExpireDt).format(
-            "DDMMMYY")}-${_.last(nameParts)}-${_.head(nameParts)}/P1`.toUpperCase(),
+        saber_: `-${_.head(nameParts)}/${_.last(nameParts)} ${gTitle(data[i])}`,
+        amadeusSRDOC: `SRDOCSXXHK1-P-${issueCountry}-${
+          passengers?.[i]?.passportNumber
+        }-${nationality}-${moment(passengers?.[i]?.birthDate).format(
+          'DDMMMYY'
+        )}-${passengers?.[i]?.gender?.substring(0, 1)}-${moment(
+          passengers?.[i]?.passExpireDt
+        ).format('DDMMMYY')}-${_.last(nameParts)}-${_.head(
+          nameParts
+        )}/P1`.toUpperCase(),
         saberSRDOC: `Not Implemented`,
+        mofaNumberBarcode: <Barcode value={data[i].mofaNumber} displayValue={false} />,
       });
     }
 
-    function gTitle(passenger, g = "amadeus") {
-      if (!passenger.birthDate) return "XX";
-      if (!passenger.gender) return "XX";
-      const age = moment().diff(passenger.birthDate, "years");
-      if (age <= 2) return "INF";
-      if (age <= 12) return "CHD";
-      if (passenger.gender === "Male") return "MR";
-      if (passenger.gender === "Female") return "MRS";
-      return "XXX";
+    function gTitle(passenger, g = 'amadeus') {
+      if (!passenger.birthDate) return 'XX';
+      if (!passenger.gender) return 'XX';
+      const age = moment().diff(passenger.birthDate, 'years');
+      if (age <= 2) return 'INF';
+      if (age <= 12) return 'CHD';
+      if (passenger.gender === 'Male') return 'MR';
+      if (passenger.gender === 'Female') return 'MRS';
+      return 'XXX';
     }
 
     return formattedPassengers;
   }
-  const [title, setTitle] = useState("Customers");
+  const [title, setTitle] = useState('Customers');
   const [showInput, setShowInput] = useState(false);
   const classes = useStyles();
   const [columns, setColumns] = useState([
     {
-      Header: "Seq",
-      accessor: "seq",
+      Header: 'Seq',
+      accessor: 'seq',
     },
     {
-      Header: "Name",
-      accessor: "name",
+      Header: 'Name',
+      accessor: 'name',
     },
     {
-      Header: "Gender",
-      accessor: "gender",
+      Header: 'Gender',
+      accessor: 'gender',
     },
     {
-      Header: "Passport Number",
-      accessor: "passportNumber",
+      Header: 'Passport Number',
+      accessor: 'passportNumber',
     },
     {
-      Header: "Birth Date",
-      accessor: "birthDate",
+      Header: 'Birth Date',
+      accessor: 'birthDate',
     },
   ]);
-  const [openSaveModal, setOpenSaveModal] = useState("");
-  const [saveReportName, setSaveReportName] = useState("");
+  const [openSaveModal, setOpenSaveModal] = useState('');
+  const [saveReportName, setSaveReportName] = useState('');
   const inputRef = useRef(null);
   const printTableRef = useRef();
   const dispatch = useDispatch();
@@ -129,7 +146,7 @@ const EmbassyReports = ({ passengers }) => {
       <Modal
         open={openSaveModal}
         onClose={() => {
-          setSaveReportName("");
+          setSaveReportName('');
           setOpenSaveModal(false);
         }}
       >
@@ -143,21 +160,21 @@ const EmbassyReports = ({ passengers }) => {
                 setSaveReportName(value);
               }}
               label="Report Name"
-              style={{ width: "100%", marginBottom: "1rem" }}
+              style={{ width: '100%', marginBottom: '1rem' }}
             />
           </Grid>
           <Grid
             item
             xs={12}
-            style={{ display: "flex", justifyContent: "flex-end" }}
+            style={{ display: 'flex', justifyContent: 'flex-end' }}
           >
             <Button
               style={{
-                background: "rgb(227, 242, 253)",
-                textTransform: "none",
-                color: "#03a9f4",
-                paddingLeft: "2rem",
-                paddingRight: "2rem",
+                background: 'rgb(227, 242, 253)',
+                textTransform: 'none',
+                color: '#03a9f4',
+                paddingLeft: '2rem',
+                paddingRight: '2rem',
               }}
               onClick={() => {
                 dispatch(
@@ -166,7 +183,7 @@ const EmbassyReports = ({ passengers }) => {
                     reportData: { columns },
                   })
                 );
-                setSaveReportName("");
+                setSaveReportName('');
                 setOpenSaveModal(false);
               }}
             >
@@ -189,7 +206,7 @@ const EmbassyReports = ({ passengers }) => {
               onChange={(e) => setTitle(e.target.value)}
             />
           ) : (
-            <Typography style={{ display: "inline" }}> {title} </Typography>
+            <Typography style={{ display: 'inline' }}> {title} </Typography>
           )}
           <IconButton
             onClick={() => {
@@ -209,9 +226,9 @@ const EmbassyReports = ({ passengers }) => {
           xs={6}
           md={6}
           style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
           }}
         >
           <ReactToPrint
@@ -228,8 +245,12 @@ const EmbassyReports = ({ passengers }) => {
         </Grid>
       </Grid>
 
-      <div style={{ display: "none" }}>
-        <PrintableTable ref={printTableRef} columns={columns} data={formatPassengers(data)} />
+      <div style={{ display: 'none' }}>
+        <PrintableTable
+          ref={printTableRef}
+          columns={columns}
+          data={formatPassengers(data)}
+        />
       </div>
 
       <Table
@@ -237,11 +258,11 @@ const EmbassyReports = ({ passengers }) => {
         data={formatPassengers(data)}
         onFilterColumn={(isFiltering) => {
           if (isFiltering) {
-            setColumns((prev) => [{ Header: "", accessor: "delete" }, ...prev]);
-            setData((prev) => prev.map((d) => ({ ...d, delete: "" })));
+            setColumns((prev) => [{ Header: '', accessor: 'delete' }, ...prev]);
+            setData((prev) => prev.map((d) => ({ ...d, delete: '' })));
           } else {
             setColumns((prev) =>
-              prev.filter((val) => val.accessor !== "delete")
+              prev.filter((val) => val.accessor !== 'delete')
             );
             setData((prev) => {
               delete prev.delete;
--- src/features/onlineCaravan/components/CustomerGender.js
+++ src/features/onlineCaravan/components/CustomerGender.js
@@ -1,13 +1,47 @@
 import { faKaaba, faMosque, faRoute } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from '@material-ui/core';
-import Grid from "@material-ui/core/Grid";
-import { ErrorMessage, Field } from "formik";
-import React from "react";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { ErrorMessage, Field } from 'formik';
+import React from 'react';
 
-const HAjj = () => <Grid container spacing={2} alignItems="center"><Grid item><FontAwesomeIcon size="1x" icon={faKaaba} color="#757575" /></Grid><Grid item><Typography>Hajj</Typography></Grid></Grid>;
-const Umrah = () => <Grid container spacing={2} alignItems="center"><Grid item><FontAwesomeIcon size="1x" icon={faMosque} color="#757575" /></Grid><Grid item><Typography>Umrah</Typography></Grid></Grid>;
-const Tour = () => <Grid container spacing={2} alignItems="center"><Grid item><FontAwesomeIcon size="1x" icon={faRoute} color="#757575" /></Grid><Grid item><Typography>Tour</Typography></Grid></Grid>;
+const HAjj = () => (
+  <Grid container spacing={2} alignItems="center">
+    <Grid item>
+      <FontAwesomeIcon size="1x" icon={faKaaba} color="#757575" />
+    </Grid>
+    <Grid item>
+      <Typography>Hajj</Typography>
+    </Grid>
+  </Grid>
+);
+const Umrah = () => (
+  <Grid container spacing={2} alignItems="center">
+    <Grid item>
+      <FontAwesomeIcon size="1x" icon={faMosque} color="#757575" />
+    </Grid>
+    <Grid item>
+      <Typography>Umrah</Typography>
+    </Grid>
+  </Grid>
+);
+const Tour = () => (
+  <Grid container spacing={2} alignItems="center">
+    <Grid item>
+      <FontAwesomeIcon size="1x" icon={faRoute} color="#757575" />
+    </Grid>
+    <Grid item>
+      <Typography>Tour</Typography>
+    </Grid>
+  </Grid>
+);
 
 const Gender = ({
   name,
@@ -15,10 +49,10 @@ const Gender = ({
   label,
   xsWidth = 4,
   autoFocus = false,
-  value = "Umrah",
+  value = 'Umrah',
   required = false,
   maxLength = 60,
-  onChange
+  onChange,
 }) => {
   return (
     <Grid item xs={xsWidth}>
@@ -27,14 +61,35 @@ const Gender = ({
         <Field
           as={RadioGroup}
           name="gender"
-          disabled={mode === "delete"}
-          value={value || ""}
+          disabled={mode === 'delete'}
+          value={value || ''}
           row
         >
           <Grid container spacing={4} alignItems="center">
-            <Grid item><FormControlLabel value="Hajj" onClick={()=> onChange('hajj')} control={<Radio />} label={<HAjj />} /></Grid>
-            <Grid item><FormControlLabel value="Umrah" onClick={()=> onChange('umrah')}  control={<Radio />} label={<Umrah />} /></Grid>
-            <Grid item><FormControlLabel value="Tour" onClick={()=> onChange('tour')}  control={<Radio />} label={<Tour />} /></Grid>
+            <Grid item>
+              <FormControlLabel
+                value="Hajj"
+                onClick={() => onChange('hajj')}
+                control={<Radio />}
+                label={<HAjj />}
+              />
+            </Grid>
+            <Grid item>
+              <FormControlLabel
+                value="Umrah"
+                onClick={() => onChange('umrah')}
+                control={<Radio />}
+                label={<Umrah />}
+              />
+            </Grid>
+            <Grid item>
+              <FormControlLabel
+                value="Tour"
+                onClick={() => onChange('tour')}
+                control={<Radio />}
+                label={<Tour />}
+              />
+            </Grid>
           </Grid>
         </Field>
       </FormControl>
