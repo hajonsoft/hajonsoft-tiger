8dd854c7711958b5f93cbb4c1410265a6eec389f
aali-bright <ayman@brightlive.com>
2022-02-07 04:41:05 -0800

    hajgy-2022

---
+++ src/firebaseConfigs/hajgy-2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hajgy-2022",
+  "appId": "1:349746788455:web:2c682f317a8b0d8f173cc2",
+  "databaseURL": "https://hajgy-2022-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "hajgy-2022.appspot.com",
+  "locationId": "asia-southeast1",
+  "apiKey": "AIzaSyD1RAhhhQcrC4tyF0eevjf2in32jrMsZ3k",
+  "authDomain": "hajgy-2022.firebaseapp.com",
+  "messagingSenderId": "349746788455"
+};
+export default firebaseConfig;
+
