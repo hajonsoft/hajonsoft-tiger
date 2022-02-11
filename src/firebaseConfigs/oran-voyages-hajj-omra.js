c1d3e60d8e6d4fe12d160c1584fb7a25492fbfe6
aali-bright <ayman@brightlive.com>
2022-02-11 12:48:41 -0800

    oran-voyages-hajj-omra

---
+++ src/firebaseConfigs/oran-voyages-hajj-omra.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "oran-voyages-hajj-omra",
+  "appId": "1:502374271264:web:b07cb9ca2b86eb91452f45",
+  "databaseURL": "https://oran-voyages-hajj-omra-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "oran-voyages-hajj-omra.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyDJ7T7BsCE4c5C1IzJd0u8cj9SvHIQSM0c",
+  "authDomain": "oran-voyages-hajj-omra.firebaseapp.com",
+  "messagingSenderId": "502374271264",
+  "measurementId": "G-SYZDESRZGX"
+};
+export default firebaseConfig;
+
