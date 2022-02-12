a891c9d62c3375adf08391b67bb60f10d9175375
aali-bright <ayman@brightlive.com>
2022-02-12 09:53:55 -0800

    daman-voyages

---
+++ src/firebaseConfigs/daman-voyages.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "daman-voyages",
+  "appId": "1:271911245852:web:1fd8005f77469396dae7d9",
+  "databaseURL": "https://daman-voyages-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "daman-voyages.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBiscmSzD6KyEXobact4ju3c7fv9H3xfeI",
+  "authDomain": "daman-voyages.firebaseapp.com",
+  "messagingSenderId": "271911245852",
+  "measurementId": "G-Q8HE6BYQ9X"
+};
+export default firebaseConfig;
+
