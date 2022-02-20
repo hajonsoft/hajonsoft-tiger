382335070cac9ac4dfd675f90c9628ad6ceda2bd
HAJonSoft <aliayman@alumni.harvard.edu>
2022-02-20 02:13:19 -0800

    THEMES (#323)
    🐯 Themes

--- src/features/Header/DoveHeader.js
+++ src/features/Header/DoveHeader.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { AppBar, Toolbar, Grid, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, Grid, Typography, Button, MenuItem, Box, Select } from '@material-ui/core';
 import { useHistory } from 'react-router';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import _ from 'lodash';
 
 import firebaseConfig from '../../firebaseConfig';
 import firebase from '../../firebaseapp';
 import t from '../../shared/util/trans';
 
-const DoveHeader = () => {
+const DoveHeader = ({ theme, themes, onThemeChange }) => {
     const projectName = `${_.startCase(firebaseConfig.projectId.replace(/[0-9]/g, '').replace(/-/g, ' '))}`;
     const history = useHistory()
     const [profile, setProfile] = useState({ name: projectName, tel: 'xxx-xxx-xxxx' })
@@ -22,22 +22,29 @@ const DoveHeader = () => {
     }, [])
 
     return (
-        <AppBar position="static" style={{ backgroundColor: 'white', boxShadow: "none" }}>
+        <AppBar position="static" style={{ backgroundColor: theme.primary, boxShadow: "none" }}>
             <Helmet>
                 <title>{`🕊️| ${profile.name}`}</title>
             </Helmet>
             <Toolbar>
                 <Grid container justifyContent="space-between" alignItems="center">
-                    <Grid item><Typography onClick={() => history.push("/")} variant="h4" align="center" style={{fontFamily: "Roboto", color: "#4caf50", cursor: "pointer" }}>{profile.name}</Typography></Grid>
+                    <Grid item><Typography onClick={() => history.push("/")} variant="h4" align="center" style={{ color: theme.color4, cursor: "pointer" }}>{profile.name}</Typography></Grid>
                     <Grid item >
-                        <Grid container spacing={2} justifyContent="center">
-                            <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/')}>{t('home.home')}</Button></Grid>
-                            <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/hajj-packages')}>{t('hajj')}</Button></Grid>
-                            <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/umrah-packages')}>{t('umrah')}</Button></Grid>
-                            <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/tours')}>{t('tours')}</Button></Grid>
-                            <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/login')}>{t('admin-login')}</Button></Grid>
-                            {/* <Grid item><Button style={{color: "#4caf50"}} onClick={() => history.push('/contact')}>contact us</Button></Grid> */}
-                        </Grid>
+                        <Box style={{ padding: '8px 32px', borderRadius: '16px', margin: '16px', backgroundColor: theme.color4, color: theme.color5 }}>
+                            <Grid container spacing={2} justifyContent="center">
+                                <Grid item><Button style={{ color: theme.tertiary, textTransform: 'none' }} onClick={() => history.push('/')}>{t('home.home')}</Button></Grid>
+                                <Grid item><Button style={{ color: theme.tertiary, textTransform: 'none' }} onClick={() => history.push('/hajj-packages')}>{t('hajj')}</Button></Grid>
+                                <Grid item><Button style={{ color: theme.tertiary, textTransform: 'none' }} onClick={() => history.push('/umrah-packages')}>{t('umrah')}</Button></Grid>
+                                <Grid item><Button style={{ color: theme.tertiary, textTransform: 'none' }} onClick={() => history.push('/tours')}>{t('tours')}</Button></Grid>
+                                <Grid item>
+                                    <Select onChange={onThemeChange} value={theme || 0} style={{ color: theme.color4 }} fullWidth label="Theme">
+                                        {Object.keys(themes).map((key,index) => <MenuItem value={index}>{key}</MenuItem>)}
+                                    </Select>
+                                </Grid>
+                                <Grid item><Button style={{ color: theme.tertiary, textTransform: 'none' }} onClick={() => history.push('/login')}>{t('admin-login')}</Button></Grid>
+                            </Grid>
+                        </Box>
+
                     </Grid>
                 </Grid>
             </Toolbar>
--- src/features/Home/DoveHome.js
+++ src/features/Home/DoveHome.js
@@ -1,37 +1,112 @@
 import { Grid } from "@material-ui/core";
-import { CalendarTodayOutlined } from "@material-ui/icons";
-import React from "react";
+import { Alert } from "@material-ui/lab";
+import React, { useState } from "react";
 import DoveHeader from "../Header/DoveHeader";
 import Advertisements from "../onlineCaravan/components/Advertisements";
 import Footer from "./components/Footer";
 import ImportantDates from "./components/importantDates";
 
+const themes = {
+  default: {
+    primary: '#005477',
+    secondary: '#ff8880',
+    tertiary: '#b30c00',
+    color4: '#ffa100',
+    color5: '#0088a1',
+  },
+  morning: {
+    primary: '#8db600',
+    secondary: '#91b300',
+    tertiary: '#b480ff',
+    color4: '#e7ff80',
+    color5: '#ff80ba',
+  },
+  lake: {
+    primary: '#03a9f4',
+    secondary: '#0085a3',
+    tertiary: '#ff8a80',
+    color4: '#0085a3',
+    color5: '#ff1400',
+  },
+  mountain: {
+    primary: '#9e9e9e',
+    secondary: '#006b6b',
+    tertiary: '#009999',
+    color4: '#ccff80',
+    color5: '#ffbfbf',
+  },
+  rocks: {
+    primary: '#607d8b',
+    secondary: '#ffc4bf',
+    tertiary: '#ffa300',
+    color4: '#80e8ff',
+    color5: '#0085a3',
+  },
+  nature: {
+    primary: '#4caf50',
+    secondary: 'olive',
+    tertiary: 'lime',
+    color4: 'indigo',
+    color5: 'yellow',
+  },
+  wood: {
+    primary: '#caa472',
+    secondary: '#400099',
+    tertiary: '#ffd980',
+    color4: '#00487d',
+    color5: '#ffecbf',
+  },
+  leather: {
+    primary: '#a80e00',
+    secondary: '#77ff00',
+    tertiary: '#bbff80',
+    color4: '#005f70',
+    color5: '#ffc4bf',
+  },
+  berry: {
+    primary: '#4f86f7',
+    secondary: '#80c9ff',
+    tertiary: '#ffb200',
+    color4: '#00487d',
+    color5: '#ff3300',
+  }
+}
+
 const DoveHome = ({ lang, onLanguageChange }) => {
+
+  const [theme, setTheme] = useState(getStoredTheme() || 0)
+
+  function getStoredTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (typeof (storedTheme) == 'string') {
+      return storedTheme
+    }
+  }
+
+  const handleOnThemeChange = (e) => {
+    setTheme(e.target.value);
+    localStorage.setItem('theme', e.target.value);
+  }
   return (
     <Grid
       container
       direction="column"
     >
       <Grid item>
-        <DoveHeader />
+        <DoveHeader theme={themes[Object.keys(themes)[theme]]} themes={themes} onThemeChange={handleOnThemeChange} />
       </Grid>
       <Grid
         item
         md={12}
       >
-        <Grid container spacing={2} alignItems="center" justifyContent="space-around" style={{backgroundColor: 'lime', color: '#8d006b', padding: '0 2rem'}}>
-          <Grid item>
-            <CalendarTodayOutlined />
-          </Grid>
-          <Grid item md={11}>
-            <ImportantDates />
-          </Grid>
-        </Grid>
+        <Alert color="info">
+          <ImportantDates />
+        </Alert>
       </Grid>
       <Grid
         item
         style={{
-          background: "rgb(63 113 136 / 9%)",
+          backgroundColor: themes[Object.keys(themes)[theme]].secondary,
         }}
       >
         <Advertisements />
