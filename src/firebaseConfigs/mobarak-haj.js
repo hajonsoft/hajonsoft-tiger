971b4c6af731b9650726bb8356cf934c4d6b72c6
aali-bright <ayman@brightlive.com>
2022-03-05 13:43:03 -0800

    mobarak-haj

--- src/firebaseConfigs/mobarak-haj.js
+++ src/firebaseConfigs/mobarak-haj.js
@@ -2,7 +2,7 @@
 // You will also need to load the Firebase SDK.
 // See https://firebase.google.com/docs/web/setup for more details.
 
-firebase.initializeApp({
+const firebaseConfig = {
   "projectId": "mobarak-haj",
   "appId": "1:463376790326:web:d4fe41b27244db0ffe7997",
   "databaseURL": "https://mobarak-haj-default-rtdb.europe-west1.firebasedatabase.app",
@@ -11,5 +11,6 @@ firebase.initializeApp({
   "apiKey": "AIzaSyDRVJlreyy7aEOaOan0dFyHxRhug_yCuSc",
   "authDomain": "mobarak-haj.firebaseapp.com",
   "messagingSenderId": "463376790326"
-});
+};
+export default firebaseConfig;
 
