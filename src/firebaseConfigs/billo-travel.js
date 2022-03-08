074d3e650de30c4a5463886f79ed95f36ae0ead6
Breno Capaz <brenocapaz@gmail.com>
2022-03-08 23:33:18 +0100

    billo-travel

---
+++ src/firebaseConfigs/billo-travel.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "billo-travel",
+  "appId": "1:199251588999:web:3f58fd88feb3f2af458522",
+  "databaseURL": "https://billo-travel-default-rtdb.firebaseio.com",
+  "storageBucket": "billo-travel.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCpbGmoxD8MtkhGhiiHXFZN4eaDmIKttqw",
+  "authDomain": "billo-travel.firebaseapp.com",
+  "messagingSenderId": "199251588999"
+};
+export default firebaseConfig;
+
