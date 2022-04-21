0abdfd26168333a89b35903c73f70e3027999510
aliayman <aliayman@alumni.harvard.edu>
2022-04-21 15:50:39 -0700

    coho-hajj

---
+++ src/firebaseConfigs/coho-hajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "coho-hajj",
+  "appId": "1:384030847513:web:6832a5fb4689df84677088",
+  "databaseURL": "https://coho-hajj-default-rtdb.firebaseio.com",
+  "storageBucket": "coho-hajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCfUpGo2Xp2EQUPLrz8Yzu7YAygbdxyb98",
+  "authDomain": "coho-hajj.firebaseapp.com",
+  "messagingSenderId": "384030847513"
+};
+export default firebaseConfig;
+
