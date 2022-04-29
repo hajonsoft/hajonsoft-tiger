63a3bff43fd17fdeff4f3b39b7b27907322c3e91
Babatunde Ololade <babatundeololade50@gmail.com>
2022-04-30 00:02:48 +0300

    add bulk JSON upload (#358)
    * add bulk JSON upload
    
    * upload photo one by one
    
    * passport and photo bulk reserve upload design
    
    * mrz scanner logo
    
    * mrz scanner logo
    
    Co-authored-by: Breno Capaz <brenocapaz@gmail.com>
    Co-authored-by: aliayman <aliayman@alumni.harvard.edu>

---
+++ src/features/BulkUpload/index.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import {Box, Button} from '@material-ui/core'
+import MaterialTable from 'material-table';
+import { tableIcons } from '../Dashboard';
+import firebase from '../../firebaseapp';
+import UploadPhoto from './uploadPhoto';
+import { nationalities } from '../../data/nationality';
+import moment from 'moment';
+
+const BulkUpload = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const params = new URLSearchParams(location.search);
+  const passportJSON = JSON.parse(JSON.stringify(params.get('json')));
+  const [photoMode, setPhotoMode] = useState(false);
+  const [passportsDetails, setPassportsDetails] = useState(
+    JSON.parse(passportJSON).map((passportDetail) => ({
+      name: passportDetail.given_names_readable,
+      gender: passportDetail.sex === 'M' ? 'Male' : 'Female',
+      nationality: nationalities.find(n => n.code === passportDetail.nationality)?.name,
+      passportNumber: passportDetail.document_number,
+      passPlaceOfIssue: passportDetail.issuing_country,
+      passIssueDt: moment(passportDetail.est_issuing_date_readable,'dd.MM.yyyy').format('YYYY-MM-DD'),
+      passExpireDt: moment(passportDetail.expiration_date_readable,'dd.MM.yyyy').format('YYYY-MM-DD'),
+      birthDate: moment(passportDetail.dob_readable,'dd.MM.yyyy').format('YYYY-MM-DD'),
+      birthPlace: passportDetail.nationality,
+    }))
+  );
+
+  const packageName = params.get('caravan');
+
+  function savePassengers(passportsDetails, packageName) {
+    const reservationReference = firebase
+      .database()
+      .ref(`customer/online`);
+  
+    passportsDetails.forEach((passport) => {
+      reservationReference.push({
+        ...passport,
+        packageName,
+      });
+    });
+    setPhotoMode(true)
+  }
+
+  return (
+    <div style={{ maxWidth: '100%', padding: '1rem' }}>
+      {photoMode && <UploadPhoto passportsDetails={passportsDetails} />}
+
+      {!photoMode && <>
+      <Box style={{display: 'flex', justifyContent: 'flex-end', padding: '16px'}}>
+        <Button variant="contained" onClick={() => savePassengers(passportsDetails, packageName, history)} color="primary" size="large">Continue ... </Button>
+      </Box>
+      <MaterialTable
+        options={{
+          search: false,
+        }}
+        icons={tableIcons}
+        cellEditable={{
+          cellStyle: {},
+          onCellEditApproved: (newValue, _, rowData) => {
+            return new Promise((resolve) => {
+              const updatedPassportDetails = passportsDetails.map(
+                (passport, index) => {
+                  if (index === Number(rowData.tableData.id)) {
+                    return {
+                      ...passport,
+                      [rowData.tableData.editCellList[0].field]: newValue,
+                    };
+                  }
+                  return passport;
+                }
+              );
+
+              setPassportsDetails(updatedPassportDetails);
+
+              setTimeout(resolve, 200);
+            });
+          },
+        }}
+        columns={[
+          { title: 'Full Name', field: 'name' },
+          { title: 'Gender', field: 'gender' },
+          { title: 'Nationality', field: 'nationality' },
+          { title: 'Passport Number', field: 'passportNumber' },
+          { title: 'Passport Issued At', field: 'passPlaceOfIssue' },
+          { title: 'Passport Issued Date', field: 'passIssueDt' },
+          { title: 'Passport Expiry Date', field: 'passExpireDt' },
+          { title: 'Birth Date', field: 'birthDate' },
+          { title: 'Birth Place', field: 'birthPlace' },
+          //   { title: 'Passport Image', field: 'passportImage' },
+          //   { title: 'Vaccine Image', field: 'vaccineImage' },
+        ]}
+        data={passportsDetails}
+        title="Bulk Reservation"
+      />
+      </>}
+    </div>
+  );
+};
+
+export default BulkUpload;
+
+
--- src/features/onlineCaravan/components/AdvertisementCard.js
+++ src/features/onlineCaravan/components/AdvertisementCard.js
@@ -4,11 +4,29 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { packageImage } from "../../../shared/util/packageImage";
-import t from '../../../shared/util/trans';
+import t from "../../../shared/util/trans";
+import { Box, Modal } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import mrzScanner from "../../../assets/mrz-scanner.png";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -21,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   },
   cardContent: {
     flexGrow: 1,
-    minHeight: "150px"
+    minHeight: "150px",
   },
   title: {
     fontSize: "1rem",
@@ -42,14 +60,76 @@ const useStyles = makeStyles((theme) => ({
     boxOrient: "vertical",
     overflow: "hidden",
   },
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #fff",
+    borderRadius: "8px",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
+    gap: "8px",
+  },
 }));
+
+// const TEMP_JSON = `[
+//   {
+//     "issuing_country" : "EGY",
+//     "nationality" : "EGY",
+//     "sex" : "M",
+//     "surname" : "Shoman",
+//     "dob_readable" : "17.08.1988",
+//     "expiration_date_readable" : "29.05.2017",
+//     "given_names_readable" : "Islam Mahmoud Mahmoud Mohamed",
+//     "expiration_date_raw" : "170529",
+//     "est_issuing_date_raw" : "100530",
+//     "optionals" : "",
+//     "document_type_readable" : "Passport",
+//     "document_type_raw" : "P",
+//     "est_issuing_date_readable" : "30.05.2010",
+//     "document_number" : "A01805459",
+//     "dob_raw" : "880817"
+//   },
+//   {
+//     "issuing_country" : "EGY",
+//     "nationality" : "EGY",
+//     "sex" : "M",
+//     "surname" : "Hassadj",
+//     "dob_readable" : "11.12.1987",
+//     "expiration_date_readable" : "23.04.2017",
+//     "given_names_readable" : "Moameej Mohamed Ahmed Eldawy",
+//     "expiration_date_raw" : "170423",
+//     "est_issuing_date_raw" : "100424",
+//     "optionals" : "",
+//     "document_type_readable" : "Passport",
+//     "document_type_raw" : "P",
+//     "est_issuing_date_readable" : "24.04.2010",
+//     "document_number" : "A01571313",
+//     "dob_raw" : "871211"
+//   }
+// ]`
 
 const AdvertisementCard = ({ detail, index }) => {
   const history = useHistory();
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const [reserveJSON, setReserveJSON] = useState('');
+
   if (!detail) {
     return null;
   }
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Card className={classes.card}>
@@ -59,11 +139,7 @@ const AdvertisementCard = ({ detail, index }) => {
         title={detail.name}
       />
       <CardContent className={classes.cardContent}>
-        <Typography
-          gutterBottom
-          variant="h5"
-          className={classes.title}
-        >
+        <Typography gutterBottom variant="h5" className={classes.title}>
           {detail.name}
         </Typography>
         <Typography variant="h1" className={classes.subTitle}>
@@ -83,18 +159,65 @@ const AdvertisementCard = ({ detail, index }) => {
           color="primary"
           variant="contained"
           onClick={() => history.push(`/reserve/${detail.name}`)}
-          style={{textTransform: 'none'}}
+          style={{ textTransform: "none" }}
         >
-          {t('reservation.reserve')}
+          {t("reservation.reserve")}
+        </Button>
+        <Button
+          title={`Make reservation to ${detail.name}`}
+          color="info"
+          variant="contained"
+          onClick={handleOpen}
+          style={{ textTransform: "none" }}
+          endIcon={<img src={mrzScanner} width="32" height="32" alt="mrz scanner app" />}
+        >
+          MRZ Scanner
         </Button>
         <Button
           title={`Learn more about ${detail.name} package`}
           onClick={() => history.push(`/package/detail/${detail.name}`)}
-          style={{textTransform: 'none'}}
+          style={{ textTransform: "none" }}
         >
-          {t('learn-more')}
+          {t("learn-more")}
         </Button>
       </CardActions>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+          <Box style={getModalStyle()} className={classes.paper}>
+            <Box style={{ width: '100%'}}>
+              <Typography gutterBottom> MRZ Scanner app</Typography>
+            </Box>
+            <TextField
+              label="Paste JSON from MRZ Scanner app here"
+              multiline
+              value={reserveJSON}
+              onChange={(e) => setReserveJSON(e.target.value)}
+              fullWidth
+              rows={4}
+              variant="filled"
+            />
+            <Button
+              title={`Make reservation to ${detail.name}`}
+              color="primary"
+              variant="contained"
+              onClick={() => {
+                if (reserveJSON) {
+                  history.push(
+                    `/bulk-reserve?json=${reserveJSON}&caravan=${detail.name}`
+                  );
+                }
+              }}
+              style={{ textTransform: "none" }}
+            >
+              Continue
+            </Button>
+          </Box>
+      </Modal>
     </Card>
   );
 };
