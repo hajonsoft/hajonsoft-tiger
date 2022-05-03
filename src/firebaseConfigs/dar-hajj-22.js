86c339e6fcd5035951455eb036183498319a378b
aliayman <aliayman@alumni.harvard.edu>
2022-05-03 09:15:11 -0700

    dar-hajj-22

---
+++ src/firebaseConfigs/dar-hajj-22.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "dar-hajj-22",
+  "appId": "1:889059770681:web:a913825d20e80bebe628e8",
+  "databaseURL": "https://dar-hajj-22-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "dar-hajj-22.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCFueJAaq3shrAESEdfPA6kHpeWtwxR56o",
+  "authDomain": "dar-hajj-22.firebaseapp.com",
+  "messagingSenderId": "889059770681",
+  "measurementId": "G-HFQD41ZVKC"
+};
+export default firebaseConfig;
+
