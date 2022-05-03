af09934d007000f2ea820cb5b9d12e7d6ea1e357
aliayman <aliayman@alumni.harvard.edu>
2022-05-03 09:10:50 -0700

    alphatours-111cc

---
+++ src/firebaseConfigs/alphatours-111cc.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alphatours-111cc",
+  "appId": "1:369578483962:web:307007c0d0df682a89e943",
+  "databaseURL": "https://alphatours-111cc-default-rtdb.firebaseio.com",
+  "storageBucket": "alphatours-111cc.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCLpRTZkAmU4p36GC67I1B8x7nTwmeDRpA",
+  "authDomain": "alphatours-111cc.firebaseapp.com",
+  "messagingSenderId": "369578483962"
+};
+export default firebaseConfig;
+
