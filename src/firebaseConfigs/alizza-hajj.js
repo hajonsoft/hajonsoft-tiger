69ce3a731fdbc233f9568dc66f4140ac447bd72d
aliayman <aliayman@alumni.harvard.edu>
2022-05-07 17:29:17 -0700

    alizza-hajj

---
+++ src/firebaseConfigs/alizza-hajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alizza-hajj",
+  "appId": "1:972382146844:web:e99262680bf522b1286ad9",
+  "databaseURL": "https://alizza-hajj-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "alizza-hajj.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAvVoexXFP0nMA0VtN0xhVKJ2V2HC-LXQA",
+  "authDomain": "alizza-hajj.firebaseapp.com",
+  "messagingSenderId": "972382146844"
+};
+export default firebaseConfig;
+
