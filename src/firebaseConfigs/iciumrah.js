fc754092574c6a12742f32e77ae79647a839d03e
aliayman <aliayman@alumni.harvard.edu>
2022-05-09 06:01:15 -0700

    iciumrah

---
+++ src/firebaseConfigs/iciumrah.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "iciumrah",
+  "appId": "1:878194847341:web:03399233af6d635ea09964",
+  "databaseURL": "https://iciumrah-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "iciumrah.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCxUFVEUFI7lLjQrDHXSwl2r67nNDCSB6w",
+  "authDomain": "iciumrah.firebaseapp.com",
+  "messagingSenderId": "878194847341",
+  "measurementId": "G-LBEPCM57LN"
+};
+export default firebaseConfig;
+
