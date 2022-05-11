cd0860edf9e058fa896b6cda291805da339d4b81
aliayman <aliayman@alumni.harvard.edu>
2022-05-11 04:53:06 -0700

    hos-igmgreisen

---
+++ src/firebaseConfigs/hos-igmgreisen.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hos-igmgreisen",
+  "appId": "1:528576607689:web:ceae18f5da4ee06f796418",
+  "databaseURL": "https://hos-igmgreisen-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "hos-igmgreisen.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAtmv5yKq6Js5bD_k12x6_1Py3Z-gp5C6g",
+  "authDomain": "hos-igmgreisen.firebaseapp.com",
+  "messagingSenderId": "528576607689"
+};
+export default firebaseConfig;
+
