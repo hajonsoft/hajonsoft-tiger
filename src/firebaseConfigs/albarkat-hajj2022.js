6dd0e19c2acba438422e531c4bf10366c5740d45
aliayman <aliayman@alumni.harvard.edu>
2022-05-12 14:04:54 -0700

    albarkat-hajj2022

---
+++ src/firebaseConfigs/albarkat-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "albarkat-hajj2022",
+  "appId": "1:569502524100:web:ca07ccfe09994c6b631f91",
+  "databaseURL": "https://albarkat-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "albarkat-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyD-5ybeyRw-cP-a0v5ZGkZadLPGc7f96jw",
+  "authDomain": "albarkat-hajj2022.firebaseapp.com",
+  "messagingSenderId": "569502524100"
+};
+export default firebaseConfig;
+
