f1e21ce01bc71ea3d75df7279d3767909266a569
aliayman <aliayman@alumni.harvard.edu>
2022-05-12 10:28:49 -0700

    alghofran

---
+++ src/firebaseConfigs/alghofran.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alghofran",
+  "appId": "1:82849770204:web:c41c56e9b0b80f3de66219",
+  "databaseURL": "https://alghofran-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "alghofran.appspot.com",
+  "locationId": "europe-central2",
+  "apiKey": "AIzaSyB_ZRwXSO5hy0d34VdkJqCpTh1AxFYDdbo",
+  "authDomain": "alghofran.firebaseapp.com",
+  "messagingSenderId": "82849770204",
+  "measurementId": "G-8DE0GV3F7C"
+};
+export default firebaseConfig;
+
