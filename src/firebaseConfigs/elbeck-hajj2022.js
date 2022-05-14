05f53702edcaa3d77323c7fb13dcd5f79bf0cf08
aliayman <aliayman@alumni.harvard.edu>
2022-05-14 12:35:16 -0700

    elbeck-hajj2022

---
+++ src/firebaseConfigs/elbeck-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "elbeck-hajj2022",
+  "appId": "1:84033529305:web:c37564dac9756cd65b25c8",
+  "databaseURL": "https://elbeck-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "elbeck-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDBpdeCsw7i7NN5jTjwYKOrOdXaiM99q7o",
+  "authDomain": "elbeck-hajj2022.firebaseapp.com",
+  "messagingSenderId": "84033529305"
+};
+export default firebaseConfig;
+
