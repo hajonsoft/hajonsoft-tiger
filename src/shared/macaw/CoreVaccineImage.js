d21b29d0acdd9915bfbe586255f50bb78a4960e3
Babatunde50 <babatundeololade50@gmail.com>
2022-05-15 23:53:16 +0300

    update core images

--- src/shared/macaw/CoreVaccineImage.js
+++ src/shared/macaw/CoreVaccineImage.js
@@ -1,41 +1,48 @@
-import { Card, CardContent, CircularProgress, Grid, Link, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import React, { useEffect, useState } from "react";
-import firebase from "../../firebaseapp";
+import {
+  Card,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Link,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import firebase from '../../firebaseapp';
 
 const useStyles = makeStyles({
   mainContainer: {
-    borderRadius: "8px",
-    position: "relative",
-    width: "90%",
-    height: "310px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "1rem"
+    borderRadius: '8px',
+    position: 'relative',
+    width: '90%',
+    height: '310px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '1rem',
   },
   imgContainer: {
-    width: "100%",
-    height: "300px",
+    width: '100%',
+    height: '300px',
   },
   pickImage: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    color: "blue",
-    fontSize: "0.8em",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'blue',
+    fontSize: '0.8em',
   },
   progress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
 });
 
 const CoreVaccineImage = ({ record, setImage, suffix = 'vaccine' }) => {
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState('');
   const [loading, setLoading] = useState(true);
   const [isMouseOver, setIsMouseOver] = useState(false);
 
@@ -51,6 +58,12 @@ const CoreVaccineImage = ({ record, setImage, suffix = 'vaccine' }) => {
 
   useEffect(() => {
     async function getImage() {
+      if (record.images?.[suffix]) {
+        setUrl(record.images[suffix]);
+        setLoading(false);
+        return;
+      }
+
       if (record && record.nationality && record.passportNumber) {
         try {
           let imgUrl = await firebase
@@ -83,39 +96,48 @@ const CoreVaccineImage = ({ record, setImage, suffix = 'vaccine' }) => {
         onMouseLeave={() => setIsMouseOver(false)}
       >
         <CardContent>
-          {!loading && <div>
-            <img
-              src={url}
-              alt={suffix}
-              className={classes.imgContainer}
-              style={{ display: url ? "block" : "none" }}
-            ></img>
-            <Link
-              href="#"
-              className={classes.pickImage}
-              style={{ display: isMouseOver && record.nationality && record.passportNumber ? "block" : "none" }}
-              onClick={() => _fileInput.click()}
-              disabled={!record.nationality || !record.passportNumber}
-            >
-              {`Change ${suffix} image`}
-            </Link>
-          </div>}
-          {loading &&
+          {!loading && (
+            <div>
+              <img
+                src={url}
+                alt={suffix}
+                className={classes.imgContainer}
+                style={{ display: url ? 'block' : 'none' }}
+              ></img>
+              <Link
+                href="#"
+                className={classes.pickImage}
+                style={{
+                  display:
+                    isMouseOver && record.nationality && record.passportNumber
+                      ? 'block'
+                      : 'none',
+                }}
+                onClick={() => _fileInput.click()}
+                disabled={!record.nationality || !record.passportNumber}
+              >
+                {`Change ${suffix} image`}
+              </Link>
+            </div>
+          )}
+          {loading && (
             <Grid container justifyContent="center" alignItems="center">
               <Grid item>
                 <CircularProgress size={80} color="secondary" />
               </Grid>
             </Grid>
-          }
+          )}
         </CardContent>
       </Card>
       <div>
-        <Typography variant="h6" component={"span"}>{suffix}</Typography>
+        <Typography variant="h6" component={'span'}>
+          {suffix}
+        </Typography>
       </div>
       <input
         type="file"
         onChange={handleFileOnChange}
-        style={{ display: "none" }}
+        style={{ display: 'none' }}
         ref={(fileInput) => (_fileInput = fileInput)}
       />
     </React.Fragment>
