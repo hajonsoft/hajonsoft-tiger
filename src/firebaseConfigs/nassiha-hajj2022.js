6735e9b29fc3daa816e7cae4ac828738868f90a0
aliayman <aliayman@alumni.harvard.edu>
2022-05-16 15:36:30 -0700

    nassiha-hajj2022

---
+++ src/firebaseConfigs/nassiha-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "nassiha-hajj2022",
+  "appId": "1:1073121147983:web:3d916d096f75db05803e3a",
+  "databaseURL": "https://nassiha-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "nassiha-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyALooAz1Wefb4e87OiL9Q6dmwvCScC_s6I",
+  "authDomain": "nassiha-hajj2022.firebaseapp.com",
+  "messagingSenderId": "1073121147983"
+};
+export default firebaseConfig;
+
