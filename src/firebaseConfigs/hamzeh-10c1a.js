6b1d08b35c3b6d93bc679a0cdfb1eef789be2c0c
aliayman <aliayman@alumni.harvard.edu>
2022-05-16 15:28:34 -0700

    hamzeh-10c1a

---
+++ src/firebaseConfigs/hamzeh-10c1a.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hamzeh-10c1a",
+  "appId": "1:728888275441:web:b0cddaa41598ecd2ff9450",
+  "databaseURL": "https://hamzeh-10c1a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "hamzeh-10c1a.appspot.com",
+  "locationId": "australia-southeast1",
+  "apiKey": "AIzaSyDLhZ9XUPdSChXavR5bSvZI4xbPCZc0MPY",
+  "authDomain": "hamzeh-10c1a.firebaseapp.com",
+  "messagingSenderId": "728888275441"
+};
+export default firebaseConfig;
+
