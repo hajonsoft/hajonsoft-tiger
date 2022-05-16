9257e1e610a3906152a32f3c1854fb4055ce4a85
aliayman <aliayman@alumni.harvard.edu>
2022-05-16 15:20:22 -0700

    alharamein-hajj2022

---
+++ src/firebaseConfigs/alharamein-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alharamein-hajj2022",
+  "appId": "1:235563865007:web:c88859d4c050cdef88555a",
+  "databaseURL": "https://alharamein-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "alharamein-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCEcmCBRjJIbCfqJB1NYNyMdOARXaZ1owc",
+  "authDomain": "alharamein-hajj2022.firebaseapp.com",
+  "messagingSenderId": "235563865007"
+};
+export default firebaseConfig;
+
