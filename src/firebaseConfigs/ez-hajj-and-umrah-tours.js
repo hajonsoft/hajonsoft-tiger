7a46fde6288030547e74b4e46d96e6e2a0a74bd3
aliayman <aliayman@alumni.harvard.edu>
2022-05-17 16:13:14 -0700

    ez-hajj-and-umrah-tours

---
+++ src/firebaseConfigs/ez-hajj-and-umrah-tours.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "ez-hajj-and-umrah-tours",
+  "appId": "1:118838265519:web:c352258440bae21c54365a",
+  "databaseURL": "https://ez-hajj-and-umrah-tours-default-rtdb.firebaseio.com",
+  "storageBucket": "ez-hajj-and-umrah-tours.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCtDRp17ca_j8b2cfgzeRXrdAm-V2CSP_8",
+  "authDomain": "ez-hajj-and-umrah-tours.firebaseapp.com",
+  "messagingSenderId": "118838265519",
+  "measurementId": "G-LNDW8ELJCD"
+};
+export default firebaseConfig;
+
