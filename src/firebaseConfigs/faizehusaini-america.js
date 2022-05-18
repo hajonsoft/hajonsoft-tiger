f6105fb5dadc8133fb8a5df5ce406e9065957167
aliayman <aliayman@alumni.harvard.edu>
2022-05-18 14:42:08 -0700

    faizehusaini-america

---
+++ src/firebaseConfigs/faizehusaini-america.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "faizehusaini-america",
+  "appId": "1:495005060761:web:f9d8cd91fbd48d001df208",
+  "databaseURL": "https://faizehusaini-america-default-rtdb.firebaseio.com",
+  "storageBucket": "faizehusaini-america.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAR1oS1Dq8ebVKKylLNPHwTEiJO5-NtGBc",
+  "authDomain": "faizehusaini-america.firebaseapp.com",
+  "messagingSenderId": "495005060761",
+  "measurementId": "G-BBQ9BZEM3C"
+};
+export default firebaseConfig;
+
