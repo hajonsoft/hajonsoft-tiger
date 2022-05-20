d1e42cd2db1e5e123eab66fe120b61956614338f
aliayman <aliayman@alumni.harvard.edu>
2022-05-20 06:19:32 -0700

    peacetravelhajj

---
+++ src/firebaseConfigs/peacetravelhajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "peacetravelhajj",
+  "appId": "1:356944717720:web:366f10498bb653e8cbaded",
+  "databaseURL": "https://peacetravelhajj-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "peacetravelhajj.appspot.com",
+  "locationId": "us-west3",
+  "apiKey": "AIzaSyDveDhaWwbRJs0dx49Ee9dW9nNorUUMV-0",
+  "authDomain": "peacetravelhajj.firebaseapp.com",
+  "messagingSenderId": "356944717720"
+};
+export default firebaseConfig;
+
