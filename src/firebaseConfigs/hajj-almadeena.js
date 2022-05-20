d81e5b36353a45eae2194ef7dcedafba053eea55
Breno Capaz <brenocapaz@gmail.com>
2022-05-19 19:04:24 -0600

    hajj-almadeena

---
+++ src/firebaseConfigs/hajj-almadeena.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hajj-almadeena",
+  "appId": "1:610811995887:web:74cd88ef873b396df11d1a",
+  "databaseURL": "https://hajj-almadeena-default-rtdb.firebaseio.com",
+  "storageBucket": "hajj-almadeena.appspot.com",
+  "locationId": "northamerica-northeast1",
+  "apiKey": "AIzaSyDAxfaOyrBYVPUWE8rexaGo6rsXvNDo8oo",
+  "authDomain": "hajj-almadeena.firebaseapp.com",
+  "messagingSenderId": "610811995887"
+};
+export default firebaseConfig;
+
