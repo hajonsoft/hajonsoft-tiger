c4a785fa93dd98361967fe56722eb4aedd077e5d
Breno Capaz <brenocapaz@gmail.com>
2022-05-19 19:07:50 -0600

    haj1443

---
+++ src/firebaseConfigs/haj1443.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "haj1443",
+  "appId": "1:108805871960:web:ea0d891c3a1087da19ad2d",
+  "databaseURL": "https://haj1443-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "haj1443.appspot.com",
+  "locationId": "asia-east1",
+  "apiKey": "AIzaSyABQ05yOuX-y9-NskZV2jQQQs5JqCGzE_E",
+  "authDomain": "haj1443.firebaseapp.com",
+  "messagingSenderId": "108805871960"
+};
+export default firebaseConfig;
+
