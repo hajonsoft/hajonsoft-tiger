2d817edaa950dbb8329e6618f5d6bab75e0c0b22
aliayman <aliayman@alumni.harvard.edu>
2022-05-21 14:27:21 -0700

    mtt-hajj2022

---
+++ src/firebaseConfigs/mtt-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "mtt-hajj2022",
+  "appId": "1:624734116778:web:9d0b2fe918769eb84429d3",
+  "databaseURL": "https://mtt-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "mtt-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBcRz505Kfwe0sir1ZOoJiVPqHAYVp6yCs",
+  "authDomain": "mtt-hajj2022.firebaseapp.com",
+  "messagingSenderId": "624734116778"
+};
+export default firebaseConfig;
+
