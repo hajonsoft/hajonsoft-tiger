53a272851e98a7b31984b9b6fbcf636dba52d669
aliayman <aliayman@alumni.harvard.edu>
2022-05-22 11:04:24 -0700

    sotra-tourisme

---
+++ src/firebaseConfigs/sotra-tourisme.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "sotra-tourisme",
+  "appId": "1:264316527627:web:6b7b663b34f0702968530e",
+  "databaseURL": "https://sotra-tourisme-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "sotra-tourisme.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyB0nX-8fZBtfxZ9unGAOK5QrFS_Wp0SOCk",
+  "authDomain": "sotra-tourisme.firebaseapp.com",
+  "messagingSenderId": "264316527627"
+};
+export default firebaseConfig;
+
