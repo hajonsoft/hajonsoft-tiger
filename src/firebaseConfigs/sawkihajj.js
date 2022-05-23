82bc049ac3673956000b5c98a8b9eb8d16de7caf
aliayman <aliayman@alumni.harvard.edu>
2022-05-23 11:13:52 -0700

    sawkihajj

---
+++ src/firebaseConfigs/sawkihajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "sawkihajj",
+  "appId": "1:555268918587:web:0673015c2b1eba8b19fb1e",
+  "databaseURL": "https://sawkihajj-default-rtdb.firebaseio.com",
+  "storageBucket": "sawkihajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAtHsqDMfPkY3tfmaSAq4Oihw4_RPgP85Y",
+  "authDomain": "sawkihajj.firebaseapp.com",
+  "messagingSenderId": "555268918587"
+};
+export default firebaseConfig;
+
