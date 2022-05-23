66937f35a24707f07a754e790ddb07189aa229ca
aliayman <aliayman@alumni.harvard.edu>
2022-05-23 08:58:26 -0700

    glorious-tours

---
+++ src/firebaseConfigs/glorious-tours.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "glorious-tours",
+  "appId": "1:877466875623:web:3ee71be80eac8fc6751442",
+  "databaseURL": "https://glorious-tours-default-rtdb.firebaseio.com",
+  "storageBucket": "glorious-tours.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyC2On9p7Eq5GSE5j7wsT1-ORCdeFSvO9pc",
+  "authDomain": "glorious-tours.firebaseapp.com",
+  "messagingSenderId": "877466875623"
+};
+export default firebaseConfig;
+
