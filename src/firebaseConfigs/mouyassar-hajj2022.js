0de40f2acacfd16f08cdf531cd1314b6f3547e56
aliayman <aliayman@alumni.harvard.edu>
2022-05-24 13:33:22 -0700

    mouyassar-hajj2022

---
+++ src/firebaseConfigs/mouyassar-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "mouyassar-hajj2022",
+  "appId": "1:854563872532:web:ac3d667fb02dd16833ec01",
+  "databaseURL": "https://mouyassar-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "mouyassar-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBNtZIRHj3ZFykWl-eQVtO_VAsC308gFHE",
+  "authDomain": "mouyassar-hajj2022.firebaseapp.com",
+  "messagingSenderId": "854563872532"
+};
+export default firebaseConfig;
+
