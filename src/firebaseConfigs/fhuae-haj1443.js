9d9f7bb5bf1546faf502a77300290ac023ecab3e
aliayman <aliayman@alumni.harvard.edu>
2022-05-24 10:27:15 -0700

    fhuae-haj1443

---
+++ src/firebaseConfigs/fhuae-haj1443.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "fhuae-haj1443",
+  "appId": "1:955227545299:web:d9b9a96bb11a266288be26",
+  "databaseURL": "https://fhuae-haj1443-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "fhuae-haj1443.appspot.com",
+  "locationId": "asia-east1",
+  "apiKey": "AIzaSyAb9VG2r9617INMXJp2nUwHIg901iUj_5E",
+  "authDomain": "fhuae-haj1443.firebaseapp.com",
+  "messagingSenderId": "955227545299"
+};
+export default firebaseConfig;
+
