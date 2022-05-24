e89cc8c6523f60028d84c44e4f5d41f482732416
aliayman <aliayman@alumni.harvard.edu>
2022-05-23 20:10:43 -0700

    mka-2022

---
+++ src/firebaseConfigs/mka-2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "mka-2022",
+  "appId": "1:250240654154:web:9079998c327a5037ed3fb3",
+  "databaseURL": "https://mka-2022-default-rtdb.firebaseio.com",
+  "storageBucket": "mka-2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCV2W7XoPtNBm222WUtdp3HKbYnXhaXH-c",
+  "authDomain": "mka-2022.firebaseapp.com",
+  "messagingSenderId": "250240654154"
+};
+export default firebaseConfig;
+
