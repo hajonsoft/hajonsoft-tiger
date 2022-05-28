cc5cf87e20a01ba767893bfa7b28351593d7cb98
aliayman <aliayman@alumni.harvard.edu>
2022-05-28 11:52:58 -0700

    tours92forhajj

---
+++ src/firebaseConfigs/tours92forhajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "tours92forhajj",
+  "appId": "1:1001360147350:web:fcaf83763ff208727653b7",
+  "databaseURL": "https://tours92forhajj-default-rtdb.firebaseio.com",
+  "storageBucket": "tours92forhajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBGtff983O1HCrHMgdIQswwD4RhQ1G6O0U",
+  "authDomain": "tours92forhajj.firebaseapp.com",
+  "messagingSenderId": "1001360147350"
+};
+export default firebaseConfig;
+
