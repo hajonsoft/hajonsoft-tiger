630992fa048f682aa5e43b9f631c8d152120f238
aliayman <aliayman@alumni.harvard.edu>
2022-05-28 08:40:08 -0700

    alkawali-hajj2022

---
+++ src/firebaseConfigs/alkawali-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alkawali-hajj2022",
+  "appId": "1:687152616140:web:378dc80568630b58a0a869",
+  "databaseURL": "https://alkawali-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "alkawali-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyC2HivjuoKF8w-LE7NST7ki8in0RHxPFzc",
+  "authDomain": "alkawali-hajj2022.firebaseapp.com",
+  "messagingSenderId": "687152616140"
+};
+export default firebaseConfig;
+
