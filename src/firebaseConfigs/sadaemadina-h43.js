9dbc9d73013a044a96eb07ded9ce9f4a9fd4f885
aliayman <aliayman@alumni.harvard.edu>
2022-05-30 06:32:52 -0700

    sadaemadina-h43

---
+++ src/firebaseConfigs/sadaemadina-h43.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "sadaemadina-h43",
+  "appId": "1:67645949861:web:1c724ebb9441ac6a048a4c",
+  "databaseURL": "https://sadaemadina-h43-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "sadaemadina-h43.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCCYCEyPagUYQF1uLQ2pvtp6bKnCdDVOQo",
+  "authDomain": "sadaemadina-h43.firebaseapp.com",
+  "messagingSenderId": "67645949861"
+};
+export default firebaseConfig;
+
