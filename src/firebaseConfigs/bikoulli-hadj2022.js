b4579af87c49802ebf024f28f80e244709bfd4a6
aliayman <aliayman@alumni.harvard.edu>
2022-05-30 07:29:49 -0700

    bikoulli-hadj2022

---
+++ src/firebaseConfigs/bikoulli-hadj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "bikoulli-hadj2022",
+  "appId": "1:378541898581:web:6bbde74ea575e820bc4ddc",
+  "databaseURL": "https://bikoulli-hadj2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "bikoulli-hadj2022.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAB-3MHo0vxJakBbaCjQpr0l-djU_R0hrQ",
+  "authDomain": "bikoulli-hadj2022.firebaseapp.com",
+  "messagingSenderId": "378541898581"
+};
+export default firebaseConfig;
+
