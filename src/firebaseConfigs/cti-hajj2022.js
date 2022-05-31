abef2c16546e7404301eb5b04e1013a0f3149dcf
aliayman <aliayman@alumni.harvard.edu>
2022-05-31 15:43:47 -0700

    cti-hajj2022

---
+++ src/firebaseConfigs/cti-hajj2022.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "cti-hajj2022",
+  "appId": "1:217903977505:web:f601cbee224ec524493f60",
+  "databaseURL": "https://cti-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "cti-hajj2022.appspot.com",
+  "locationId": "northamerica-northeast1",
+  "apiKey": "AIzaSyDIlahmMjnfIJHr6972sQlzA7smQZrRYH4",
+  "authDomain": "cti-hajj2022.firebaseapp.com",
+  "messagingSenderId": "217903977505",
+  "measurementId": "G-8FS4WZ1FVM"
+};
+export default firebaseConfig;
+
