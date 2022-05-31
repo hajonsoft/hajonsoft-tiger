33f73db38c71b8eab9f15bbd4d024f11eec10748
aliayman <aliayman@alumni.harvard.edu>
2022-05-31 09:08:54 -0700

    babmakka-hajj

---
+++ src/firebaseConfigs/babmakka-hajj.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "babmakka-hajj",
+  "appId": "1:838202430240:web:c42a8e734357a00c953d45",
+  "databaseURL": "https://babmakka-hajj-default-rtdb.firebaseio.com",
+  "storageBucket": "babmakka-hajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDc63m9-0o5DajmRqROzgddaPFJoxB72U4",
+  "authDomain": "babmakka-hajj.firebaseapp.com",
+  "messagingSenderId": "838202430240",
+  "measurementId": "G-WPB3VH7XRM"
+};
+export default firebaseConfig;
+
