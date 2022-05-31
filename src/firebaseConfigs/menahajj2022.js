dc92e9f816a786bdcd46d8b135a59161ff95c657
aliayman <aliayman@alumni.harvard.edu>
2022-05-30 18:23:17 -0700

    menahajj2022

---
+++ src/firebaseConfigs/menahajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "menahajj2022",
+  "appId": "1:729099802984:web:5baf8524c509c9daecdedd",
+  "databaseURL": "https://menahajj2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "menahajj2022.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAqYqF6aUjsprmK-uODTWFx2A3vTgw5E8s",
+  "authDomain": "menahajj2022.firebaseapp.com",
+  "messagingSenderId": "729099802984"
+};
+export default firebaseConfig;
+
