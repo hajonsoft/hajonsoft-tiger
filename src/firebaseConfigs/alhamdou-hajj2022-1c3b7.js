5a4f9a651108a474ed38191a4f40061d0d01ce0d
aliayman <aliayman@alumni.harvard.edu>
2022-05-31 06:39:29 -0700

    alhamdou-hajj2022-1c3b7

---
+++ src/firebaseConfigs/alhamdou-hajj2022-1c3b7.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alhamdou-hajj2022-1c3b7",
+  "appId": "1:126573621642:web:4107a1f4e2b36597b19873",
+  "databaseURL": "https://alhamdou-hajj2022-1c3b7-default-rtdb.firebaseio.com",
+  "storageBucket": "alhamdou-hajj2022-1c3b7.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCZAnEIa7pMNsfIjVdZe28pR11iWxkKwII",
+  "authDomain": "alhamdou-hajj2022-1c3b7.firebaseapp.com",
+  "messagingSenderId": "126573621642"
+};
+export default firebaseConfig;
+
