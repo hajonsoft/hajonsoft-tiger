3afa395701db0610420d49d68c58fd4c6b880c3c
aliayman <aliayman@alumni.harvard.edu>
2022-05-30 18:20:43 -0700

    alaqsa-hajj

---
+++ src/firebaseConfigs/alaqsa-hajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alaqsa-hajj",
+  "appId": "1:54930028862:web:7084e03b0ad72c9c0d546f",
+  "databaseURL": "https://alaqsa-hajj-default-rtdb.firebaseio.com",
+  "storageBucket": "alaqsa-hajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyB-rX8jZP5lJOrPXStqCtZpZNBlGoWT9LU",
+  "authDomain": "alaqsa-hajj.firebaseapp.com",
+  "messagingSenderId": "54930028862"
+};
+export default firebaseConfig;
+
