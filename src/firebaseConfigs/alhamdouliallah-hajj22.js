773c85f6db23a2c894ac721adf79fcc8248e4f11
aliayman <aliayman@alumni.harvard.edu>
2022-06-01 16:36:37 -0700

    alhamdouliallah-hajj22

---
+++ src/firebaseConfigs/alhamdouliallah-hajj22.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alhamdouliallah-hajj22",
+  "appId": "1:296412757490:web:9e986ec01d818d95993f6b",
+  "databaseURL": "https://alhamdouliallah-hajj22-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "alhamdouliallah-hajj22.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBZ9c8LofSyyLhniGDnp4Rig3xOwObZ1Xc",
+  "authDomain": "alhamdouliallah-hajj22.firebaseapp.com",
+  "messagingSenderId": "296412757490"
+};
+export default firebaseConfig;
+
