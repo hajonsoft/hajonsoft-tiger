0c2114cd148d0e062c6887fca7e15317373a8e66
aliayman <aliayman@alumni.harvard.edu>
2022-06-01 12:37:33 -0700

    Maison-du-hadj

---
+++ src/firebaseConfigs/Maison-du-hadj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "orga-voyages-2022",
+  "appId": "1:48287385982:web:1a8e308bcaa99c653a8b2f",
+  "databaseURL": "https://orga-voyages-2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "orga-voyages-2022.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCV0hsya5wE-ohRqWzRm1gxOWVJyE9zG8o",
+  "authDomain": "orga-voyages-2022.firebaseapp.com",
+  "messagingSenderId": "48287385982"
+};
+export default firebaseConfig;
+
