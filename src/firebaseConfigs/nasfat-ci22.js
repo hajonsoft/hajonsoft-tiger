7687647454cf0532c16de56cd67850b7a53bb4ab
aliayman <aliayman@alumni.harvard.edu>
2022-06-01 13:04:06 -0700

    nasfat-ci22

---
+++ src/firebaseConfigs/nasfat-ci22.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "nasfat-ci22",
+  "appId": "1:734847372510:web:3355d88d3f3ed9965c864f",
+  "databaseURL": "https://nasfat-ci22-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "nasfat-ci22.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCOFokQMXqGTX0BU-HinspaG43UP0OMo_s",
+  "authDomain": "nasfat-ci22.firebaseapp.com",
+  "messagingSenderId": "734847372510"
+};
+export default firebaseConfig;
+
