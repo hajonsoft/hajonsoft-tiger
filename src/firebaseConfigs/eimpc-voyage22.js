a7804e710a57398a8095a60102aeb23c1315e9d5
aliayman <aliayman@alumni.harvard.edu>
2022-06-01 16:30:48 -0700

    eimpc-voyage22

---
+++ src/firebaseConfigs/eimpc-voyage22.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "eimpc-voyage22",
+  "appId": "1:154762047236:web:f9b86eef825fbf1d7daa51",
+  "databaseURL": "https://eimpc-voyage22-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "eimpc-voyage22.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyBSSAoBB68vIpIIK8Ru8Lc1Rt2KaM0GmSY",
+  "authDomain": "eimpc-voyage22.firebaseapp.com",
+  "messagingSenderId": "154762047236"
+};
+export default firebaseConfig;
+
