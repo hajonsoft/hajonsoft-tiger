a4ac43531d0870c8e874a01985a88e8db4a798c6
aliayman <aliayman@alumni.harvard.edu>
2022-05-31 22:22:15 -0700

    sky2022-3dfbb

---
+++ src/firebaseConfigs/sky2022-3dfbb.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "sky2022-3dfbb",
+  "appId": "1:484792051605:web:83a6c31a3152ec462c389c",
+  "databaseURL": "https://sky2022-3dfbb-default-rtdb.firebaseio.com",
+  "storageBucket": "sky2022-3dfbb.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDlxgVXnu13bhP0adGbdu_5P-3jpYag9BE",
+  "authDomain": "sky2022-3dfbb.firebaseapp.com",
+  "messagingSenderId": "484792051605"
+};
+export default firebaseConfig;
+
