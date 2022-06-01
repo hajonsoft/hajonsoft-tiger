58dc55b5e8f05b21cdd6eb0e33319c340a805588
aliayman <aliayman@alumni.harvard.edu>
2022-06-01 07:34:45 -0700

    foulanzam-hajj

---
+++ src/firebaseConfigs/foulanzam-hajj.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "foulanzam-hajj",
+  "appId": "1:478069837712:web:6f2dd5270f4622a7ba2d15",
+  "databaseURL": "https://foulanzam-hajj-default-rtdb.firebaseio.com",
+  "storageBucket": "foulanzam-hajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAGpHeyyEwD5RW_Z9p_brKpwOReZ2AVNhM",
+  "authDomain": "foulanzam-hajj.firebaseapp.com",
+  "messagingSenderId": "478069837712",
+  "measurementId": "G-WEYCQBTQG3"
+};
+export default firebaseConfig;
+
