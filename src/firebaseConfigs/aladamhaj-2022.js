eddf1959e7d8e0efbb29a4a670ea8ab910c6d46f
aliayman <aliayman@alumni.harvard.edu>
2022-06-02 08:19:36 -0700

    aladamhaj-2022

---
+++ src/firebaseConfigs/aladamhaj-2022.js
@@ -0,0 +1,15 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "projectId": "aladamhaj-2022",
+  "appId": "1:97452865919:web:20f07084df33dc3d28c24a",
+  "databaseURL": "https://aladamhaj-2022-default-rtdb.firebaseio.com",
+  "storageBucket": "aladamhaj-2022.appspot.com",
+  "locationId": "us-west3",
+  "apiKey": "AIzaSyDPYGXYS4KKc-aAkpIUyujlFVw64mPHbss",
+  "authDomain": "aladamhaj-2022.firebaseapp.com",
+  "messagingSenderId": "97452865919"
+});
+
