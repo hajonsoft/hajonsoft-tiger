4b2d3cc845ca62dcfcab0f99c62eaa69a1b357ea
aliayman <aliayman@alumni.harvard.edu>
2022-06-03 07:46:21 -0700

    montbagazame

---
+++ src/firebaseConfigs/montbagazame.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "montbagazame",
+  "appId": "1:297450324323:web:c8b70a4e638b859dbc5356",
+  "databaseURL": "https://montbagazame-default-rtdb.firebaseio.com",
+  "storageBucket": "montbagazame.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyA0hqzDnBgdmC4Ap2ISV2rTwN680lCuzqQ",
+  "authDomain": "montbagazame.firebaseapp.com",
+  "messagingSenderId": "297450324323",
+  "measurementId": "G-CCVG02KJ46"
+};
+export default firebaseConfig;
+
