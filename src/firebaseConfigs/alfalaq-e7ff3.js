bebe43a96a4b4d31ea65429d9503bdd7541d8537
aliayman <aliayman@alumni.harvard.edu>
2022-06-03 10:45:44 -0700

    alfalaq-e7ff3

---
+++ src/firebaseConfigs/alfalaq-e7ff3.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alfalaq-e7ff3",
+  "appId": "1:333128721279:web:fd9bdeeb179c8e6533304a",
+  "databaseURL": "https://alfalaq-e7ff3-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "alfalaq-e7ff3.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyA-Q9wrzp1YB1mdvqrntSLLsk__Btkl2VU",
+  "authDomain": "alfalaq-e7ff3.firebaseapp.com",
+  "messagingSenderId": "333128721279",
+  "measurementId": "G-GET42ZQFPN"
+};
+export default firebaseConfig;
+
