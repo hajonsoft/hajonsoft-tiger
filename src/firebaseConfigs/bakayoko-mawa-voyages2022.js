f4df3822e86473db632a817d92dc4149a8d8f200
aliayman <aliayman@alumni.harvard.edu>
2022-06-03 09:07:17 -0700

    bakayoko-mawa-voyages2022

---
+++ src/firebaseConfigs/bakayoko-mawa-voyages2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "bakayoko-mawa-voyages2022",
+  "appId": "1:222119903084:web:c30ae463b0664b1786fd49",
+  "databaseURL": "https://bakayoko-mawa-voyages2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "bakayoko-mawa-voyages2022.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyB1I7pH1eCenXVxmPb3pMHDYDkBHnPrNCQ",
+  "authDomain": "bakayoko-mawa-voyages2022.firebaseapp.com",
+  "messagingSenderId": "222119903084"
+};
+export default firebaseConfig;
+
