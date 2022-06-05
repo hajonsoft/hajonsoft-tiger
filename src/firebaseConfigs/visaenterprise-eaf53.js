9eb263da0e3ce122c6e61c6f2332aceda9f751f1
aliayman <aliayman@alumni.harvard.edu>
2022-06-04 20:44:13 -0700

    visaenterprise-eaf53

---
+++ src/firebaseConfigs/visaenterprise-eaf53.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "visaenterprise-eaf53",
+  "appId": "1:1022424647112:web:e0c7a69a17e88f893f6cfd",
+  "databaseURL": "https://visaenterprise-eaf53-default-rtdb.firebaseio.com",
+  "storageBucket": "visaenterprise-eaf53.appspot.com",
+  "locationId": "us-east1",
+  "apiKey": "AIzaSyATYgOBZf1u-9AMq6lIcYKw2sh1t0TCyeU",
+  "authDomain": "visaenterprise-eaf53.firebaseapp.com",
+  "messagingSenderId": "1022424647112",
+  "measurementId": "G-D7F2DPVDYX"
+};
+export default firebaseConfig;
+
