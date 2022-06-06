d5eaf98ce0765e0a7a243de587eecad624c98a51
aliayman <aliayman@alumni.harvard.edu>
2022-06-06 14:50:03 -0700

    lloydstravelmw

---
+++ src/firebaseConfigs/lloydstravelmw.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "lloydstravelmw",
+  "appId": "1:210987166742:web:f93423b8e4917a604fd937",
+  "databaseURL": "https://lloydstravelmw-default-rtdb.firebaseio.com",
+  "storageBucket": "lloydstravelmw.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDQopjQr3EFyn6gkdB__lCQ_AJihtnc8yU",
+  "authDomain": "lloydstravelmw.firebaseapp.com",
+  "messagingSenderId": "210987166742"
+};
+export default firebaseConfig;
+
