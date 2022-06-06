9c6c9cd1f1d44a4d461dcf7800b1baeb29b8610f
aliayman <aliayman@alumni.harvard.edu>
2022-06-05 20:43:04 -0700

    carlton-travelagency

---
+++ src/firebaseConfigs/carlton-travelagency.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "carlton-travelagency",
+  "appId": "1:713785966411:web:9c2bb18340b5d17505b2ce",
+  "databaseURL": "https://carlton-travelagency-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "carlton-travelagency.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCHvnaPb45CQ3RejXt_TlUn17TxJ3w2rzY",
+  "authDomain": "carlton-travelagency.firebaseapp.com",
+  "messagingSenderId": "713785966411"
+};
+export default firebaseConfig;
+
