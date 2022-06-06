2b7d0e81653b80e6769b455d734f41eb4bba0c98
aliayman <aliayman@alumni.harvard.edu>
2022-06-06 14:34:24 -0700

    hajj-alarabia

---
+++ src/firebaseConfigs/hajj-alarabia.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hajj-alarabia",
+  "appId": "1:322487067739:web:9f545b06d6cb4474308b7e",
+  "databaseURL": "https://hajj-alarabia-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "hajj-alarabia.appspot.com",
+  "locationId": "asia-southeast2",
+  "apiKey": "AIzaSyCeBVJ_VynDRwX8DOBWkoEuU3mFwrqtEw8",
+  "authDomain": "hajj-alarabia.firebaseapp.com",
+  "messagingSenderId": "322487067739"
+};
+export default firebaseConfig;
+
