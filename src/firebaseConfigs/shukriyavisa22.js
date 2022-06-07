86f11b956dbe395c5ca424f148e6aee46c8d13d3
aliayman <aliayman@alumni.harvard.edu>
2022-06-07 06:41:15 -0700

    shukriyavisa22

---
+++ src/firebaseConfigs/shukriyavisa22.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "shukriyavisa22",
+  "appId": "1:276056466596:web:a22646d0032ab257e9f231",
+  "databaseURL": "https://shukriyavisa22-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "shukriyavisa22.appspot.com",
+  "locationId": "asia-southeast1",
+  "apiKey": "AIzaSyB3AAVRfqv6pKuZH_frbITa95gzBYg8D4Q",
+  "authDomain": "shukriyavisa22.firebaseapp.com",
+  "messagingSenderId": "276056466596",
+  "measurementId": "G-JZWL35T2VG"
+};
+export default firebaseConfig;
+
