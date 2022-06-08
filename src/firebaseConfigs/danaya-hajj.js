277394fe4132ed1e5091b7032ceb6ba105351b15
aliayman <aliayman@alumni.harvard.edu>
2022-06-07 17:18:16 -0700

    danaya-hajj

---
+++ src/firebaseConfigs/danaya-hajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "danaya-hajj",
+  "appId": "1:958251231335:web:250573cbf980567dd08cc2",
+  "databaseURL": "https://danaya-hajj-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "danaya-hajj.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCZyIr9HVKR2XwCXNuo1p3VzQIwyYfCc6Y",
+  "authDomain": "danaya-hajj.firebaseapp.com",
+  "messagingSenderId": "958251231335"
+};
+export default firebaseConfig;
+
