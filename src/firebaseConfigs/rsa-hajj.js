9303140ce561580cc3afc39df40357d8457aa0d7
aliayman <aliayman@alumni.harvard.edu>
2022-06-09 06:07:14 -0700

    rsa-hajj

---
+++ src/firebaseConfigs/rsa-hajj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "rsa-hajj",
+  "appId": "1:391447603393:web:45034de03db43084e84c40",
+  "databaseURL": "https://rsa-hajj-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "storageBucket": "rsa-hajj.appspot.com",
+  "locationId": "asia-south1",
+  "apiKey": "AIzaSyCeOWiTEtukq-Fs4mdL8-8PHW7wrr3TDss",
+  "authDomain": "rsa-hajj.firebaseapp.com",
+  "messagingSenderId": "391447603393"
+};
+export default firebaseConfig;
+
