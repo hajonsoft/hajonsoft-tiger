52473aaf7af78ccd361a50289af1c3f06de56952
aliayman <aliayman@alumni.harvard.edu>
2022-06-09 10:48:20 -0700

    bakhlamofa2022

---
+++ src/firebaseConfigs/bakhlamofa2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "bakhlamofa2022",
+  "appId": "1:683064551932:web:f67c3938ee1d42207a6a79",
+  "databaseURL": "https://bakhlamofa2022-default-rtdb.firebaseio.com",
+  "storageBucket": "bakhlamofa2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCSedJaOEOO_GjC6GwHek7Qw6Y26Bs2iD0",
+  "authDomain": "bakhlamofa2022.firebaseapp.com",
+  "messagingSenderId": "683064551932"
+};
+export default firebaseConfig;
+
