598adff546b347db46947d5e6d72f1e963a2cb8e
aliayman <aliayman@alumni.harvard.edu>
2022-06-13 16:38:20 -0700

    fissabilillah-hajj2022

---
+++ src/firebaseConfigs/fissabilillah-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "fissabilillah-hajj2022",
+  "appId": "1:336340095499:web:5964ce1582dc3a1885ed0e",
+  "databaseURL": "https://fissabilillah-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "fissabilillah-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAUf1sFHKEdaRUD027eaxabp6OOA_3FISQ",
+  "authDomain": "fissabilillah-hajj2022.firebaseapp.com",
+  "messagingSenderId": "336340095499"
+};
+export default firebaseConfig;
+
