67704ef0ea0beb2b0667830101ab04c9f20f15b8
aliayman <aliayman@alumni.harvard.edu>
2022-06-12 18:37:23 -0700

    ehaj-support

---
+++ src/firebaseConfigs/ehaj-support.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "ehaj-support",
+  "appId": "1:302400112441:web:658f6fc51b78b5a984cae8",
+  "databaseURL": "https://ehaj-support-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "ehaj-support.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyDWGd_JQXRCefhtIqTMW4GNab096EYvr94",
+  "authDomain": "ehaj-support.firebaseapp.com",
+  "messagingSenderId": "302400112441",
+  "measurementId": "G-Q33CFYWXBT"
+};
+export default firebaseConfig;
+
