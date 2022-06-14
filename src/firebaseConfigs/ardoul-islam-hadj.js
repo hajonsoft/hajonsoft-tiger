3f8818554b6537f701950f59bc67cf2db8d88dda
aliayman <aliayman@alumni.harvard.edu>
2022-06-14 14:15:25 -0700

    ardoul-islam-hadj

---
+++ src/firebaseConfigs/ardoul-islam-hadj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "ardoul-islam-hadj",
+  "appId": "1:126592501783:web:9307a9ea180a0157f5f452",
+  "databaseURL": "https://ardoul-islam-hadj-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "ardoul-islam-hadj.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAFIQkLG2VKTJk_eQlFjBGX4cNqs9EgBck",
+  "authDomain": "ardoul-islam-hadj.firebaseapp.com",
+  "messagingSenderId": "126592501783"
+};
+export default firebaseConfig;
+
