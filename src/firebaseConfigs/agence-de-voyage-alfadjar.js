9f33b4be1cbd051a45d39cd57af2845d0df95077
aliayman <aliayman@alumni.harvard.edu>
2022-06-14 09:16:47 -0700

    agence-de-voyage-alfadjar

---
+++ src/firebaseConfigs/agence-de-voyage-alfadjar.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "agence-de-voyage-alfadjar",
+  "appId": "1:951726310234:web:a3f802c3279d025f806963",
+  "databaseURL": "https://agence-de-voyage-alfadjar-default-rtdb.firebaseio.com",
+  "storageBucket": "agence-de-voyage-alfadjar.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDRd8_lf4LTMkb1F15W8bd0dWCDDes7no4",
+  "authDomain": "agence-de-voyage-alfadjar.firebaseapp.com",
+  "messagingSenderId": "951726310234"
+};
+export default firebaseConfig;
+
