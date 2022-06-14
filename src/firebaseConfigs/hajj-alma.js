0af7af5762fc856fade1e4905334cf3b3a5adca8
aliayman <aliayman@alumni.harvard.edu>
2022-06-14 13:58:45 -0700

    hajj-alma

---
+++ src/firebaseConfigs/hajj-alma.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hajj-alma",
+  "appId": "1:757503518918:web:5328a2c82c4115db4d8fc7",
+  "databaseURL": "https://hajj-alma-default-rtdb.firebaseio.com",
+  "storageBucket": "hajj-alma.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBFJLU3HcX7qDpOCqjf6vmifZA-Qr2CxvM",
+  "authDomain": "hajj-alma.firebaseapp.com",
+  "messagingSenderId": "757503518918"
+};
+export default firebaseConfig;
+
