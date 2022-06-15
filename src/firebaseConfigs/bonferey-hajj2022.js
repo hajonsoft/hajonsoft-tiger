405b9034b4244efd512b8de47365cfba96c478e3
aliayman <aliayman@alumni.harvard.edu>
2022-06-15 16:12:02 -0700

    bonferey-hajj2022

---
+++ src/firebaseConfigs/bonferey-hajj2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "bonferey-hajj2022",
+  "appId": "1:580099016322:web:d7d4cb8e16f3e81cb0569b",
+  "databaseURL": "https://bonferey-hajj2022-default-rtdb.firebaseio.com",
+  "storageBucket": "bonferey-hajj2022.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDxZL4VcMB_YrKaCF-aBxk1nF3wk9cUCjg",
+  "authDomain": "bonferey-hajj2022.firebaseapp.com",
+  "messagingSenderId": "580099016322"
+};
+export default firebaseConfig;
+
