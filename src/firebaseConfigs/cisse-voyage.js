ef697db4348e426cc7c00f7334af2473e0241277
aliayman <aliayman@alumni.harvard.edu>
2022-06-16 14:06:58 -0700

    cisse-voyage

---
+++ src/firebaseConfigs/cisse-voyage.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "cisse-voyage",
+  "appId": "1:1071623053098:web:2d757a9ece036f95c50853",
+  "databaseURL": "https://cisse-voyage-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "cisse-voyage.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyClWptUyI4kbTR7AvYW-wnY3vOlDorJMJg",
+  "authDomain": "cisse-voyage.firebaseapp.com",
+  "messagingSenderId": "1071623053098"
+};
+export default firebaseConfig;
+
