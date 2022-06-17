3d9c541617477898d40fae8657ff65d7d9f9d5a3
aliayman <aliayman@alumni.harvard.edu>
2022-06-17 12:16:43 -0700

    almawassim-hadj

---
+++ src/firebaseConfigs/almawassim-hadj.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "almawassim-hadj",
+  "appId": "1:569831406870:web:506488f218cc3e490e2437",
+  "databaseURL": "https://almawassim-hadj-default-rtdb.firebaseio.com",
+  "storageBucket": "almawassim-hadj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCBF_3mRmE9awcNuDqsn97_ul4TTTpmE3c",
+  "authDomain": "almawassim-hadj.firebaseapp.com",
+  "messagingSenderId": "569831406870"
+};
+export default firebaseConfig;
+
