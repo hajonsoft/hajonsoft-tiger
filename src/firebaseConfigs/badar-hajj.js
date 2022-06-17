e1e7979ecb32f8cbfa31a0417ae31129b2c2ab3e
aliayman <aliayman@alumni.harvard.edu>
2022-06-17 13:41:35 -0700

    badar-hajj

---
+++ src/firebaseConfigs/badar-hajj.js
@@ -0,0 +1,17 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "badar-hajj",
+  "appId": "1:452873081678:web:aab1669b52850169ab841f",
+  "databaseURL": "https://badar-hajj-default-rtdb.firebaseio.com",
+  "storageBucket": "badar-hajj.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyBQmGfHQt7JJH-ZRcXakexd09guH5It2pI",
+  "authDomain": "badar-hajj.firebaseapp.com",
+  "messagingSenderId": "452873081678",
+  "measurementId": "G-8LM726MKWH"
+};
+export default firebaseConfig;
+
