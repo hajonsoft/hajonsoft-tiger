9b4822a1529bbe01d527d6f9b600ecd0cbba64f3
aliayman <aliayman@alumni.harvard.edu>
2022-06-19 09:07:52 -0700

    alhendy

---
+++ src/firebaseConfigs/alhendy.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "alhendy",
+  "appId": "1:464576580160:web:e04f6a14b985ae20b82da5",
+  "databaseURL": "https://alhendy-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "alhendy.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCKPLjQ7aqCurLDVYCXvZHXfNYJBo_Hgb0",
+  "authDomain": "alhendy.firebaseapp.com",
+  "messagingSenderId": "464576580160"
+};
+export default firebaseConfig;
+
