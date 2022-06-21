d0694a6478cce99cbbf96d3ae40995fa3cdfd74e
aliayman <aliayman@alumni.harvard.edu>
2022-06-20 21:59:39 -0700

    hajjdgp-2022

---
+++ src/firebaseConfigs/hajjdgp-2022.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hajjdgp-2022",
+  "appId": "1:468291613469:web:2b006cec7aaf7b813ee7f4",
+  "databaseURL": "https://hajjdgp-2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "hajjdgp-2022.appspot.com",
+  "locationId": "europe-west6",
+  "apiKey": "AIzaSyAFZSsTpvO9rnE2utmwprpo-siA2YIE0rY",
+  "authDomain": "hajjdgp-2022.firebaseapp.com",
+  "messagingSenderId": "468291613469"
+};
+export default firebaseConfig;
+
