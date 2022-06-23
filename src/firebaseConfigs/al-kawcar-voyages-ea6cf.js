7a9092a6042ad2c6e31e9a33ee4fa472cde9a106
aliayman <aliayman@alumni.harvard.edu>
2022-06-23 16:54:39 -0700

    al-kawcar-voyages-ea6cf

---
+++ src/firebaseConfigs/al-kawcar-voyages-ea6cf.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "al-kawcar-voyages-ea6cf",
+  "appId": "1:817547621898:web:dffaa5e359947aa7a511f3",
+  "databaseURL": "https://al-kawcar-voyages-ea6cf-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "al-kawcar-voyages-ea6cf.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAY1UbrVQYyn3gvRjnCwIgfuaDvy_4DeNo",
+  "authDomain": "al-kawcar-voyages-ea6cf.firebaseapp.com",
+  "messagingSenderId": "817547621898"
+};
+export default firebaseConfig;
+
