c1f65a498a69c717b6db48f193b9bd9a64d7c8ca
aliayman <aliayman@alumni.harvard.edu>
2022-06-23 17:08:02 -0700

    al-houda-voyages

---
+++ src/firebaseConfigs/al-houda-voyages.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "al-houda-voyages",
+  "appId": "1:541977243786:web:df8d9abc1a974135653d73",
+  "databaseURL": "https://al-houda-voyages-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "al-houda-voyages.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyDHJzPyI2SDtQcbUsdX_RrH1JZxAqWwBLw",
+  "authDomain": "al-houda-voyages.firebaseapp.com",
+  "messagingSenderId": "541977243786"
+};
+export default firebaseConfig;
+
