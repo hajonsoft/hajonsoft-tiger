8b67e90c9dff17cd8efba1529227cbff529762e4
aliayman <aliayman@alumni.harvard.edu>
2022-06-23 17:01:06 -0700

    vert-paradis-tours

---
+++ src/firebaseConfigs/vert-paradis-tours.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "vert-paradis-tours",
+  "appId": "1:1022963887719:web:9f8627691bd03959b36ec2",
+  "databaseURL": "https://vert-paradis-tours-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "vert-paradis-tours.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCuYkbeaM6YoAnVhViMC8ivwZaWO4h49b0",
+  "authDomain": "vert-paradis-tours.firebaseapp.com",
+  "messagingSenderId": "1022963887719"
+};
+export default firebaseConfig;
+
