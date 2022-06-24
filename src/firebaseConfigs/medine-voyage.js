b0a5ce166809c8f6096fde1decf2b4883d3394a5
aliayman <aliayman@alumni.harvard.edu>
2022-06-23 17:12:40 -0700

    medine-voyage

---
+++ src/firebaseConfigs/medine-voyage.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "medine-voyage",
+  "appId": "1:199502754633:web:a21b62755ab51f28ee2098",
+  "databaseURL": "https://medine-voyage-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "medine-voyage.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCmDWaKAppaU9hojKJ4a_WJS3KY033_IMg",
+  "authDomain": "medine-voyage.firebaseapp.com",
+  "messagingSenderId": "199502754633"
+};
+export default firebaseConfig;
+
