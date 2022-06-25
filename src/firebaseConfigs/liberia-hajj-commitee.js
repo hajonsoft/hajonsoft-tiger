eb70c1310f391db5777ad35135c1acb3986ed8f2
aliayman <aliayman@alumni.harvard.edu>
2022-06-24 20:26:46 -0700

    liberia-hajj-commitee

---
+++ src/firebaseConfigs/liberia-hajj-commitee.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "liberia-hajj-commitee",
+  "appId": "1:988913594863:web:e6361188bb65f035a0c61a",
+  "databaseURL": "https://liberia-hajj-commitee-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "liberia-hajj-commitee.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyCCUGKK0TXWU7z3axWFNTk8lA3o8-bXy2c",
+  "authDomain": "liberia-hajj-commitee.firebaseapp.com",
+  "messagingSenderId": "988913594863"
+};
+export default firebaseConfig;
+
