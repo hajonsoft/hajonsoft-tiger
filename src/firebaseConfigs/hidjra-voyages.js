e8b29fed08bb2fd62b0faa0f894746f2c26c881e
aliayman <aliayman@alumni.harvard.edu>
2022-06-26 06:35:32 -0700

    hidjra-voyages

---
+++ src/firebaseConfigs/hidjra-voyages.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "hidjra-voyages",
+  "appId": "1:1090984109916:web:78c4531e5c349e77134399",
+  "databaseURL": "https://hidjra-voyages-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "hidjra-voyages.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyAjzc7swgNDOYOBEu6e41wruOsyu3T4JsY",
+  "authDomain": "hidjra-voyages.firebaseapp.com",
+  "messagingSenderId": "1090984109916"
+};
+export default firebaseConfig;
+
