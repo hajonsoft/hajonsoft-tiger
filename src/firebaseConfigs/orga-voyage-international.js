c030c09dfc68c27dcf750f51c7bd0d03c99a59cf
aliayman <aliayman@alumni.harvard.edu>
2022-06-27 11:12:54 -0700

    orga-voyage-international

---
+++ src/firebaseConfigs/orga-voyage-international.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "orga-voyage-international",
+  "appId": "1:905515049754:web:d5cf1b70a366ce87c202a3",
+  "databaseURL": "https://orga-voyage-international-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "orga-voyage-international.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyDmuco8bi_lg8_s7SDGGa0MQVhZnjao9Vw",
+  "authDomain": "orga-voyage-international.firebaseapp.com",
+  "messagingSenderId": "905515049754"
+};
+export default firebaseConfig;
+
