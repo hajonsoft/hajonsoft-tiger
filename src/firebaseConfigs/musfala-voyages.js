be71df06a7c05d839f3ada6ab71a910f4b6c8757
aliayman <aliayman@alumni.harvard.edu>
2022-06-27 16:12:20 -0700

    musfala-voyages

---
+++ src/firebaseConfigs/musfala-voyages.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "musfala-voyages",
+  "appId": "1:120728541021:web:dd33d5c26656098f0f3ab4",
+  "databaseURL": "https://musfala-voyages-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "musfala-voyages.appspot.com",
+  "locationId": "europe-west",
+  "apiKey": "AIzaSyA240wm8D36WKEujoyh0osQbPp5L3FzVU8",
+  "authDomain": "musfala-voyages.firebaseapp.com",
+  "messagingSenderId": "120728541021"
+};
+export default firebaseConfig;
+
