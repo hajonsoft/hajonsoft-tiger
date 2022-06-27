90d540932a25de63fac2fa037e6603fd3862339e
aliayman <aliayman@alumni.harvard.edu>
2022-06-27 09:49:04 -0700

    songhaytravelagency

---
+++ src/firebaseConfigs/songhaytravelagency.js
@@ -0,0 +1,16 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+const firebaseConfig = {
+  "projectId": "songhaytravelagency",
+  "appId": "1:466161846953:web:a1ff3712600fb29a2e77b8",
+  "databaseURL": "https://songhaytravelagency-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "songhaytravelagency.appspot.com",
+  "locationId": "europe-west2",
+  "apiKey": "AIzaSyAb_bXCSBz8TyE5uGKtJ4sx19HqNbMtmJg",
+  "authDomain": "songhaytravelagency.firebaseapp.com",
+  "messagingSenderId": "466161846953"
+};
+export default firebaseConfig;
+
